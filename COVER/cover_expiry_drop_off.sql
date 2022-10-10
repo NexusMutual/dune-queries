ea34d0648ebfae6e2c148c2da311f479c34772bf
Guy Howlett <howlett_948@msn.com>
2022-10-10 21:57:30 +0100

    Fixed starting values for dai and eth
    We were starting with negative values for eth which was clearly not
    correct.
    
    Still concerned that we finish on -12Eth as well as a high dai value.

--- COVER/cover_expiry_drop_off.sql
+++ COVER/cover_expiry_drop_off.sql
@@ -18,72 +18,98 @@ with
       case
         when cover_asset = 'ETH' then -1 * sum_assured
         when cover_asset = 'DAI' then 0
-        ELSE 0
-      end as gross_eth,
+      end as gross_eth_expired,
       case
         when cover_asset = 'ETH' then 0
         when cover_asset = 'DAI' then -1 * sum_assured
-        ELSE 0
-      end as gross_dai,
+      end as gross_dai_expired,
       date_trunc('day', cover_end_time) as date_c
     from
       cover_data as s
   ),
   cover_data_buy as (
     select
-      date_trunc('day', cover_start_time) as date_c,
       cover_asset,
       case
         when cover_asset = 'ETH' then sum_assured
         when cover_asset = 'DAI' then 0
-        ELSE 0
-      end as gross_eth,
+      end as gross_eth_brought,
       case
         when cover_asset = 'ETH' then 0
         when cover_asset = 'DAI' then sum_assured
-        ELSE 0
-      end as gross_dai
+      end as gross_dai_brought,
+      date_trunc('day', cover_start_time) as date_c
     from
       cover_data
   ),
-  cover_data_summary as (
+  cover_data_buy_summed as (
     select
       date_c,
-      gross_eth,
-      gross_dai
-    from
-      cover_data_expired
-    where
-      date_c <= NOW()
-    UNION
-    select
-      date_c,
-      gross_eth,
-      gross_dai
-    from
+      SUM(gross_eth_brought) as eth_summed_buy,
+      SUM(gross_dai_brought) as dai_summed_buy
+    FROM
       cover_data_buy
+    group by
+      date_c
   ),
-  cover_expire_to_in_future as (
-    SELECT
-      1 as anchor,
+  cover_data_sold_summed as (
+    select
       date_c,
-      gross_eth,
-      gross_dai
-    from
+      SUM(gross_eth_expired) as eth_summed_expired,
+      SUM(gross_dai_expired) as dai_summed_expired
+    FROM
       cover_data_expired
-    where
-      date_c >= NOW()
+    WHERE
+      date_c < NOW()
+    group by
+      date_c
+  ),
+  cover_day_changes as (
+    SELECT
+      cover_data_buy_summed.date_c,
+      cover_data_buy_summed.eth_summed_buy,
+      cover_data_sold_summed.eth_summed_expired,
+      cover_data_buy_summed.dai_summed_buy,
+      cover_data_sold_summed.dai_summed_expired,
+      cover_data_buy_summed.eth_summed_buy + cover_data_sold_summed.eth_summed_expired as net_eth,
+      cover_data_buy_summed.dai_summed_buy + cover_data_sold_summed.dai_summed_expired as net_dai
+    from
+      cover_data_sold_summed
+      JOIN cover_data_buy_summed ON cover_data_sold_summed.date_c = cover_data_buy_summed.date_c
   ),
   starting_totals as (
     select
       1 as anchor,
-      SUM(gross_eth) as eth,
-      SUM(gross_dai) as dai
+      sum(net_eth) as starting_eth,
+      sum(net_dai) as starting_dai
+    FROM
+      cover_day_changes
+  ),
+  cover_expire_to_in_future as (
+    SELECT
+      DISTINCT date_c,
+      1 as anchor,
+      SUM(gross_eth_expired) OVER (
+        ORDER BY
+          date_c ASC
+      ) as eth_running_expiry,
+      SUM(gross_dai_expired) OVER (
+        ORDER BY
+          date_c ASC
+      ) as dai_running_expiry
     from
-      cover_data_summary
+      cover_data_expired
+    where
+      date_c >= NOW()
   )
 SELECT
-  *
+  date_c,
+  eth_running_expiry,
+  dai_running_expiry,
+  starting_eth,
+  starting_dai,
+  starting_eth + eth_running_expiry as total_eth,
+  starting_dai + dai_running_expiry as total_dai
 from
   cover_expire_to_in_future
   JOIN starting_totals ON starting_totals.anchor = cover_expire_to_in_future.anchor
