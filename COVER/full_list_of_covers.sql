5570c0be590a58fc14be1f715f9e1725e620a57f
Guy Howlett <howlett_948@msn.com>
2022-10-12 18:39:41 +0100

    Add all final headers that are currently spec'd to full_list_of_covers.sql

--- COVER/full_list_of_covers.sql
+++ COVER/full_list_of_covers.sql
@@ -3,70 +3,48 @@ WITH
     select
       "cid" as cover_id,
       "evt_block_time" as start_time,
-      date '1970-01-01 00:00:00' + concat("expiry", ' second'):: interval as end_time,
+      date '1970-01-01 00:00:00' + concat("expiry", ' second') :: interval as end_time,
       "scAdd" as user,
       "sumAssured" as sum_assured,
       case
         when "curr" = '\x45544800' then 'ETH'
         when "curr" = '\x44414900' then 'DAI'
       end as cover_asset,
-      "premiumNXM" as premiumNxm,
-      "premium" as premium,
-      "evt_tx_hash" as evt_hash 
+      "premiumNXM" * 1E-18 as premiumNxm,
+      "premium" * 1E-18 as premium,
+      "evt_tx_hash" as evt_hash
     from
       nexusmutual."QuotationData_evt_CoverDetailsEvent"
-    ),
-    covers as (
-        select
-        "_userAddress" as user_address,
-        "call_tx_hash" as call_tx
-        from
-        nexusmutual."QuotationData_call_addCover"
-        where
-        call_success = true
-    )
-SELECT
-  *,
-  case
-    when end_time >= NOW() then True
-    when end_time < NOW() then False
-  end as active
-from
-  covers
- INNER JOIN covers_details_event ON covers.call_tx= covers_details_event.evt_hash 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  with
-  add_cover as (
+  ),
+  covers as (
     select
-      "premium" as prem,
-      "call_block_time" as time
+      "_userAddress" as user_address,
+      "call_tx_hash" as call_tx
     from
       nexusmutual."QuotationData_call_addCover"
     where
       call_success = true
-  ),
-  cover_details as (
-    select
-      "evt_block_time" as time,
-      "premium" as premium
-    from
-      nexusmutual."QuotationData_evt_CoverDetailsEvent"
   )
-select
-  *
+SELECT
+  cover_id,
+  cover_asset,
+  sum_assured,
+  premiumNxm,
+  premium,
+  'v1' as syndicate,
+  'v1' as product_class,
+  'v1' as product,
+  start_time,
+  1 as start_month,
+  end_time,
+  user_address,
+  end_time - start_time as period,
+  case
+    when end_time >= NOW() then True
+    when end_time < NOW() then False
+  end as active,
+  1 as rank_no,
+  call_tx
 from
-  cover_details
-  INNER JOIN add_cover ON cover_details.time = add_cover.time
+  covers
+  INNER JOIN covers_details_event ON covers.call_tx = covers_details_event.evt_hash
