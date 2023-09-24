b78427bf9d3eee9b6e2389b7cf9ddb89cf113c79
Guy Howlett <howlett_948@msn.com>
2023-09-24 11:36:00 +0100

    Added query for a comparison of claims assessor gas fees between nexus v1 and nexus v2.

---
+++ CLAIMS/claims-gas-fees.sql
@@ -0,0 +1,74 @@
+WITH
+  vote_count AS (
+    SELECT
+      concat('V1_', CAST(claimId AS VARCHAR)) AS name,
+      claimId AS v1_claim_id,
+      NULL AS v2_claim_id,
+      evt_tx_hash,
+      CASE
+        WHEN verdict = 1 THEN true
+        WHEN verdict = -1 THEN false
+      END AS result,
+      CAST(tokens AS DOUBLE) * 1E-18 AS nxm_vote
+    FROM
+      nexusmutual_ethereum.ClaimsData_evt_VoteCast
+    UNION
+    select
+      concat('V2_', CAST(assessmentId AS VARCHAR)) AS name,
+      NULL AS v1_claim_id,
+      assessmentId AS v2_claim_id,
+      evt_tx_hash,
+      accepted,
+      CAST(stakedAmount AS DOUBLE) * 1E-18 AS nxm_vote
+    from
+      nexusmutual_ethereum.Assessment_evt_VoteCast
+  ),
+  votes_with_gas_usage AS (
+    select
+    DISTINCT
+      name,
+      v1_claim_id,
+      v2_claim_id,
+      CASE
+        WHEN v1_claim_id IS NOT NULL THEN AVG(gas_used * gas_price * 1E-18) OVER (
+          partition by
+            name
+        )
+        ELSE NULL
+      END AS v1_average_gas_cost,
+      CASE
+        WHEN v2_claim_id IS NOT NULL THEN AVG(gas_used * gas_price * 1E-18) OVER (
+          partition by
+            name
+        )
+        ELSE NULL
+      END AS v2_average_gas_cost,
+      CASE
+        WHEN v1_claim_id IS NOT NULL THEN AVG(CAST(gas_used AS double)) OVER (
+          PARTITION BY
+            name
+        )
+        ELSE NULL
+      END AS v1_gas_used,
+      CASE
+        WHEN v2_claim_id IS NOT NULL THEN AVG(CAST(gas_used AS double)) OVER (
+          PARTITION BY
+            name
+        )
+        ELSE NULL
+      END AS v2_gas_used
+    from
+      vote_count
+      INNER JOIN ethereum.transactions AS t ON vote_count.evt_tx_hash = t.hash
+    WHERE
+      block_time > CAST('2019-05-01' AS TIMESTAMP)
+  )
+select
+*,
+AVG(v1_average_gas_cost) OVER () AS v1_average_gas_cost_all,
+AVG(v2_average_gas_cost) OVER () AS v2_average_gas_cost_all
+from
+  votes_with_gas_usage
+ORDER by
+  v1_claim_id,
+  v2_claim_id
