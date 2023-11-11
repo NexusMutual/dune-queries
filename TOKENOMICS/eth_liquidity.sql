027bbcc77663c976b372b0f8fbad882a04f889e5
Guy Howlett <howlett_948@msn.com>
2023-11-11 12:49:48 +0000

    Changed to get reserves and add extraction of budget.

--- TOKENOMICS/eth_liquidity.sql
+++ TOKENOMICS/eth_liquidity.sql
@@ -1,12 +1,13 @@
 WITH
-  eth_liquidity AS (
+  get_reserves AS (
     SELECT
       date_trunc('minute', call_block_time) AS ts,
-      CAST(output_0 AS DOUBLE) AS eth_liquidity
+      CAST(output_0 AS DOUBLE) AS eth_liquidity,
+      CAST(output_3 AS DOUBLE) AS budget
     FROM
       nexusmutual_ethereum.Ramm_call_getReserves
   )
 SELECT
-*
+  *
 FROM
-eth_liquidity 
+  get_reserves
