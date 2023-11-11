fc257fbafcae62c079527ad5cca603a052ddac88
Guy Howlett <howlett_948@msn.com>
2023-11-11 12:37:33 +0000

    Add ETH -> USD conversio for book value

--- TOKENOMICS/book_value.sql
+++ TOKENOMICS/book_value.sql
@@ -1,9 +1,31 @@
-WITH book_value_eth AS (
-
+WITH
+  book_value_eth AS (
     SELECT
-        CAST(call_block_time AS TIMESTAMP) AS ts,
+      CAST(call_block_time AS TIMESTAMP) AS ts,
+      CAST(output_0 AS DOUBLE) AS book_value_eth
     FROM
-    nexusmutual_ethereum.Ramm_call_getBookValue AS a
-
-    
-)
+      nexusmutual_ethereum.Ramm_call_getBookValue AS a
+    WHERE
+      call_success
+  ),
+  prices AS (
+    SELECT DISTINCT
+      date_trunc('minute', minute) AS ts,
+      symbol,
+      AVG(price) OVER (
+        PARTITION BY
+          date_trunc('minute', minute)
+      ) AS price_dollar
+    FROM
+      prices.usd
+    WHERE
+      symbol = 'ETH'
+      AND minute > CAST('2023-11-11' AS TIMESTAMP)
+  )
+SELECT
+  a.ts,
+  book_value_eth,
+  book_value_eth * price_dollar AS book_value_dollar
+FROM
+  book_value_eth AS a
+  INNER JOIN prices AS b ON a.ts = b.ts
