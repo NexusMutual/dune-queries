b14c9529eb5aa51d2f4c8766e1f7aae0c6417570
Guy Howlett <howlett_948@msn.com>
2023-11-11 12:38:44 +0000

    Add ETH -> USD conversion for spot prices

--- TOKENOMICS/spot_price.sql
+++ TOKENOMICS/spot_price.sql
@@ -1,9 +1,32 @@
 WITH
-    spot_price AS (
-        SELECT
-            CAST(call_block_time AS TIMESTAMP) AS ts,
-             CAST(output_0 AS DOUBLE) / CAST(output_1 AS DOUBLE) AS spot_price_eth_a,
-             CAST(output_0 AS DOUBLE) / CAST(output_2 AS DOUBLE) AS spot_price_eth_b
-        FROM
-            nexusmutual_ethereum.Ramm_call_getReserves
-    )
+  spot_price AS (
+    SELECT
+      date_trunc('minute', call_block_time) AS ts,
+      CAST(output_0 AS DOUBLE) / CAST(output_1 AS DOUBLE) AS spot_price_eth_a,
+      CAST(output_0 AS DOUBLE) / CAST(output_2 AS DOUBLE) AS spot_price_eth_b
+    FROM
+      nexusmutual_ethereum.Ramm_call_getReserves
+  ),
+  prices AS (
+    SELECT DISTINCT
+      date_trunc('minute', minute) AS ts,
+      symbol,
+      AVG(price) OVER (
+        PARTITION BY
+          date_trunc('minute', minute)
+      ) AS price_dollar
+    FROM
+      prices.usd
+    WHERE
+      symbol = 'ETH'
+      AND minute > CAST('2023-11-11' AS TIMESTAMP)
+  )
+SELECT
+  a.ts,
+  spot_price_eth_a,
+  spot_price_eth_b,
+  spot_price_eth_a * price_dollar AS spot_price_dollar_a,
+  spot_price_eth_b * price_dollar AS spot_price_dollar_b,
+FROM
+  spot_price AS a
+  INNER JOIN prices AS b ON a.ts = b.ts
