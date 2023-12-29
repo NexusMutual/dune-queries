32853043d37a182fa8898497292e4c3154093068
Guy Howlett <howlett_948@msn.com>
2023-12-29 14:12:06 +0000

    Parameterise, calculate cummalative injected eth from RAMM event emission.

--- TOKENOMICS/cumulative_liquidity_injected.sql
+++ TOKENOMICS/cumulative_liquidity_injected.sql
@@ -2,13 +2,30 @@ WITH
   eth_injected AS (
     SELECT
       CAST(evt_block_time AS TIMESTAMP) AS ts,
-      CAST(injected AS DOUBLE) * 1E-18 AS eth_injected
+      CAST(value AS DOUBLE) * 1E-18 AS eth_injected
     FROM
-      nexusmutual_ethereum.QuotationData_evt_ethinjected
+      nexusmutual_ethereum.Ramm_evt_EthInjected
+  ),
+  cumulative_injected AS (
+    SELECT
+      ts,
+      eth_injected,
+      SUM(eth_injected) OVER (
+        ORDER BY
+          ts
+      ) AS cummulative_injected
+    FROM
+      eth_injected
   )
 SELECT
-  *
+  ts,
+  eth_injected,
+  cummulative_injected,
+  CAST(43850.0 AS DOUBLE) - cummulative_injected AS remaining_budget
 FROM
-  eth_injected
+  cumulative_injected
+WHERE
+  ts >= CAST('{{Start Date}}' AS TIMESTAMP)
+  AND ts <= CAST('{{End Date}}' AS TIMESTAMP)
 ORDER BY
   ts DESC
