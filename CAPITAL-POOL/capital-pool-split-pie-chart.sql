b42de344fa58b106ca94b9b4b71ec0fa2d23b87c
tomfutago <tomfutago@gmail.com>
2024-06-05 16:58:45 +0100

    capital pool split end date param

--- CAPITAL-POOL/capital-pool-split-pie-chart.sql
+++ CAPITAL-POOL/capital-pool-split-pie-chart.sql
@@ -1,18 +1,23 @@
 with
 
+capital_pool_timed as (
+  select * from nexusmutual_ethereum.capital_pool_totals
+  where block_date = if(timestamp '{{End Date}}' > current_timestamp, cast(current_date as timestamp), timestamp '{{End Date}}')
+),
+
 capital_pool_assets (asset_type, eth_amount, usd_amount, rn) as (
-  select 'ETH', eth_total, avg_eth_usd_total, 1 from nexusmutual_ethereum.capital_pool_latest union all
-  select 'DAI', avg_dai_eth_total, avg_dai_usd_total, 2 from nexusmutual_ethereum.capital_pool_latest union all
-  select 'stETH', steth_total, avg_steth_usd_total, 3 from nexusmutual_ethereum.capital_pool_latest union all
-  select 'rETH', avg_reth_eth_total, avg_reth_usd_total, 4 from nexusmutual_ethereum.capital_pool_latest union all
-  select 'NXMTY', nxmty_eth_total, avg_nxmty_usd_total, 5 from nexusmutual_ethereum.capital_pool_latest union all
-  select 'Aave WETH', aave_collateral_weth_total, avg_aave_collateral_weth_usd_total, 6 from nexusmutual_ethereum.capital_pool_latest union all
+  select 'ETH', eth_total, avg_eth_usd_total, 1 from capital_pool_timed union all
+  select 'DAI', avg_dai_eth_total, avg_dai_usd_total, 2 from capital_pool_timed union all
+  select 'stETH', steth_total, avg_steth_usd_total, 3 from capital_pool_timed union all
+  select 'rETH', avg_reth_eth_total, avg_reth_usd_total, 4 from capital_pool_timed union all
+  select 'NXMTY', nxmty_eth_total, avg_nxmty_usd_total, 5 from capital_pool_timed union all
+  select 'Aave WETH', aave_collateral_weth_total, avg_aave_collateral_weth_usd_total, 6 from capital_pool_timed union all
   select
     'USDC',
     avg_usdc_eth_total + avg_cover_re_usdc_eth_total - avg_aave_debt_usdc_eth_total,
     avg_usdc_usd_total + avg_cover_re_usdc_usd_total - avg_aave_debt_usdc_usd_total,
     7
-  from nexusmutual_ethereum.capital_pool_latest
+  from capital_pool_timed
 )
 
 select
