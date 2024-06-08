3e065fc03729645083c16f0189e9ba31005aff82
tomfutago <tomfutago@gmail.com>
2024-06-08 12:15:35 +0100

    cover start & end date amounts

--- COVERS/v1-covers.sql
+++ COVERS/v1-covers.sql
@@ -8,6 +8,8 @@ covers as (
     cover_id,
     cover_start_time,
     cover_end_time,
+    date_trunc('day', cover_start_time) as cover_start_date,
+    date_trunc('day', cover_end_time) as cover_end_date,
     product_contract,
     syndicate,
     product_name,
@@ -39,19 +41,27 @@ select
   c.cover_asset,
   c.sum_assured,
   case
-    when c.cover_asset = 'ETH' then c.sum_assured * p.avg_eth_usd_price
-    when c.cover_asset = 'DAI' then c.sum_assured * p.avg_dai_usd_price
-  end as cover_usd,
+    when c.cover_asset = 'ETH' then c.sum_assured * p_start.avg_eth_usd_price
+    when c.cover_asset = 'DAI' then c.sum_assured * p_start.avg_dai_usd_price
+  end as cover_start_usd,
   case
     when c.cover_asset = 'ETH' then c.sum_assured
-    when c.cover_asset = 'DAI' then (c.sum_assured * p.avg_dai_usd_price) / p.avg_eth_usd_price
-  end as cover_eth,
+    when c.cover_asset = 'DAI' then (c.sum_assured * p_start.avg_dai_usd_price) / p_start.avg_eth_usd_price
+  end as cover_start_eth,
+  case
+    when c.cover_asset = 'ETH' then c.sum_assured * p_end.avg_eth_usd_price
+    when c.cover_asset = 'DAI' then c.sum_assured * p_end.avg_dai_usd_price
+  end as cover_end_usd,
+  case
+    when c.cover_asset = 'ETH' then c.sum_assured
+    when c.cover_asset = 'DAI' then (c.sum_assured * p_end.avg_dai_usd_price) / p_end.avg_eth_usd_price
+  end as cover_end_eth,
   c.premium_asset,
   c.premium_nxm,
   c.premium,
   case
-    when c.cover_asset = 'ETH' then c.premium * p.avg_eth_usd_price
-    when c.cover_asset = 'DAI' then c.premium * p.avg_dai_usd_price
+    when c.cover_asset = 'ETH' then c.premium * p_start.avg_eth_usd_price
+    when c.cover_asset = 'DAI' then c.premium * p_start.avg_dai_usd_price
   end as premium_usd,
   c.syndicate,
   c.product_type,
@@ -62,5 +72,6 @@ select
   date_diff('day', c.cover_start_time, c.cover_end_time) as cover_period,
   c.tx_hash
 from covers c
-  left join daily_avg_prices p on c.block_date = p.block_date
+  left join daily_avg_prices p_start on c.cover_start_date = p_start.block_date
+  left join daily_avg_prices p_end on c.cover_end_date = p_end.block_date
 order by cover_id desc
