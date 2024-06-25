a4ef11e3f1e4bd4a0ff32c8e2fed065bd0224cba
tomfutago <tomfutago@gmail.com>
2024-06-25 14:29:55 +0100

    update pie chart buckets

--- CAPITAL-POOL/capital-pool-split-pie-chart.sql
+++ CAPITAL-POOL/capital-pool-split-pie-chart.sql
@@ -11,12 +11,13 @@ capital_pool_assets (asset_type, eth_amount, usd_amount, rn) as (
   select 'stETH', steth_total, avg_steth_usd_total, 3 from capital_pool_timed union all
   select 'rETH', avg_reth_eth_total, avg_reth_usd_total, 4 from capital_pool_timed union all
   select 'NXMTY', nxmty_eth_total, avg_nxmty_usd_total, 5 from capital_pool_timed union all
-  select 'Aave WETH', aave_collateral_weth_total, avg_aave_collateral_weth_usd_total, 6 from capital_pool_timed union all
+  select 'USDC', avg_usdc_eth_total, avg_usdc_usd_total, 6 from capital_pool_timed union all
+  select 'Cover Re', avg_cover_re_usdc_eth_total, avg_cover_re_usdc_usd_total, 7 from capital_pool_timed union all
   select
-    'USDC',
-    avg_usdc_eth_total + avg_cover_re_usdc_eth_total - avg_aave_debt_usdc_eth_total,
-    avg_usdc_usd_total + avg_cover_re_usdc_usd_total - avg_aave_debt_usdc_usd_total,
-    7
+    'Aave',
+    aave_collateral_weth_total + avg_aave_debt_usdc_eth_total,
+    avg_aave_collateral_weth_usd_total + avg_aave_debt_usdc_usd_total,
+    8
   from capital_pool_timed
 )
 
