3819783f46992d19f28ef5966973db3863287477
tomfutago <tomfutago@gmail.com>
2024-07-02 10:35:50 +0100

    name tweak

--- COVERS/active-cover-bd.sql
+++ COVERS/active-cover-bd.sql
@@ -116,8 +116,8 @@ daily_cover_enriched as (
 select
   block_date,
   cover_sold,
-  eth_eth_cover_total + dai_eth_cover_total + usdc_eth_cover_total as eth_cover_total,
-  eth_usd_cover_total + dai_usd_cover_total + usdc_usd_cover_total as usd_cover_total,
+  eth_eth_cover_total + dai_eth_cover_total + usdc_eth_cover_total as eth_active_cover_total,
+  eth_usd_cover_total + dai_usd_cover_total + usdc_usd_cover_total as usd_active_cover_total,
   eth_eth_premium_total + dai_eth_premium_total + nxm_eth_premium_total as eth_premium_total,
   eth_usd_premium_total + dai_usd_premium_total + nxm_usd_premium_total as usd_premium_total
 from daily_cover_enriched
