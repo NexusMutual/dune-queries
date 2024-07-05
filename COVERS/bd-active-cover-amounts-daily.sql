2fa90aa824262f1d7e378d4df296cb222751a0cd
tomfutago <tomfutago@gmail.com>
2024-07-05 14:19:51 +0100

    update daily & monthly stats

--- COVERS/bd-active-cover-amounts-daily.sql
+++ COVERS/bd-active-cover-amounts-daily.sql
@@ -1,23 +1,22 @@
 select
   block_date,
-  cover_sold,
+  active_cover_sold,
   eth_active_cover,
-  eth_eth_cover,
-  dai_eth_cover,
-  usdc_eth_cover,
+  eth_eth_active_cover,
+  dai_eth_active_cover,
+  usdc_eth_active_cover,
   usd_active_cover,
-  eth_usd_cover,
-  dai_usd_cover,
-  usdc_usd_cover,
-  eth_premium,
-  eth_eth_premium,
-  dai_eth_premium,
-  nxm_eth_premium,
-  usd_premium,
-  eth_usd_premium,
-  dai_usd_premium,
-  nxm_usd_premium
+  eth_usd_active_cover,
+  dai_usd_active_cover,
+  usdc_usd_active_cover,
+  eth_active_premium,
+  eth_eth_active_premium,
+  dai_eth_active_premium,
+  nxm_eth_active_premium,
+  usd_active_premium,
+  eth_usd_active_premium,
+  dai_usd_active_premium,
+  nxm_usd_active_premium
 from query_3889661
-where block_date >= timestamp '{{Start Date}}'
-  and block_date < timestamp '{{End Date}}'
+where block_date >= now() - interval '6' month
 order by 1 desc
