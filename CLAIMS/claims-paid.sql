d5134b93385fa494b8c47f7ecbd7d5f4f724983a
tomfutago <tomfutago@gmail.com>
2024-07-09 12:14:19 +0100

    combine datasets

--- CLAIMS/claims-paid.sql
+++ CLAIMS/claims-paid.sql
@@ -1,90 +1,62 @@
 with
 
-covers_v1 as (
+covers as (
   select
+    1 as version,
     cover_id,
     cover_asset,
     sum_assured
   --from query_3788367 -- covers v1 base (fallback) query
   from nexusmutual_ethereum.covers_v1
-),
-
-claims_v1 as (
-  select
-    claim_id,
-    cover_id,
-    submit_time,
-    submit_date,
-    partial_claim_amount,
-    claim_status
-  from query_3894606 -- claims v1 base (fallback) query
-  where claim_status = 14 -- ??
-),
-
-claims_paid_v1 as (
-  select
-    cl.claim_id,
-    cl.submit_date as claim_date,
-    c.cover_asset,
-    coalesce(cl.partial_claim_amount, c.sum_assured) as claim_amount
-  from covers_v1 c
-    inner join claims_v1 cl on c.cover_id = cl.cover_id
-),
-
-covers_v2 as (
+  union all
   select distinct
+    2 as version,
     cover_id,
     cover_asset,
     sum_assured
-  from query_3788370 -- covers v2 base (fallback) query
-  --from nexusmutual_ethereum.covers_v2 -- spell needs updating - add product_id
+  --from query_3788370 -- covers v2 base (fallback) query
+  from nexusmutual_ethereum.covers_v2
+  where is_migrated = false
 ),
 
-claims_v2 as (
+claims as (
   select
-    submit_time,
-    submit_date,
+    1 as version,
     claim_id,
     cover_id,
-    product_id,
-    assessment_id,
-    cover_asset,
-    requested_amount
+    submit_date as claim_date,
+    partial_claim_amount as claim_amount
+  from query_3894606 -- claims v1 base (fallback) query
+  where claim_status = 14
+    or claim_id = 102
+  union all
+  select
+    2 as version,
+    claim_id,
+    cover_id,
+    submit_date as claim_date,
+    requested_amount as claim_amount
   from query_3894982 -- claims v2 base (fallback) query
 ),
 
-claims_paid_v2 as (
+claims_paid as (
   select
+    cl.version,
     cl.claim_id,
-    cl.submit_date as claim_date,
+    claim_date,
     c.cover_asset,
-    cl.requested_amount as claim_amount
-  from covers_v2 c
-    inner join claims_v2 cl on c.cover_id = cl.cover_id
-    inner join nexusmutual_ethereum.IndividualClaims_call_redeemClaimPayout cp on cl.claim_id = cp.claimId
-  where cp.call_success
-),
-
-claims_paid AS (
-  select
-    claim_date,
-    cover_asset,
-    claim_amount
-  from claims_paid_v1
-  union all
-  select
-    claim_date,
-    cover_asset,
-    claim_amount
-  from claims_paid_v2
-  --UNION
-  --SELECT
-  --  CAST('2021-11-05 00:00' AS TIMESTAMP) AS claim_time,
-  --  10.43 AS amount,
-  --  'ETH' AS cover_asset
+    coalesce(cl.claim_amount, c.sum_assured) as claim_amount
+  from covers c
+    inner join claims cl on c.cover_id = cl.cover_id and c.version = cl.version
+    left join nexusmutual_ethereum.IndividualClaims_call_redeemClaimPayout cp on cl.claim_id = cp.claimId
+      and cl.version = 2
+      and cp.call_success
+  where cl.version = 1
+    or (cl.version = 2 and cp.claimId is not null)
 )
 
 select * from claims_paid
+
 
 prices as (
   select
@@ -98,56 +70,39 @@ prices as (
   group by 1, 2
 ),
 
-  total_claims AS (
-    SELECT
-      claim_time,
-      SUM(
-        CASE
-          WHEN cover_asset = 'DAI'
-          AND '{{display_currency}}' = 'USD' THEN amount * dai_price_dollar
-          WHEN cover_asset = 'DAI'
-          AND '{{display_currency}}' = 'ETH' THEN amount * dai_price_dollar / eth_price_dollar
-          ELSE 0
-        END
-      ) OVER (
-        ORDER BY
-          claim_time
-      ) AS running_dai_claimed,
-      SUM(
-        CASE
-          WHEN cover_asset = 'ETH'
-          AND '{{display_currency}}' = 'USD' THEN amount * eth_price_dollar
-          WHEN cover_asset = 'ETH'
-          AND '{{display_currency}}' = 'ETH' THEN amount
-          ELSE 0
-        END
-      ) OVER (
-        ORDER BY
-          claim_time
-      ) AS running_eth_claimed,
-      SUM(
-        CASE
-          WHEN cover_asset = 'ETH'
-          AND '{{display_currency}}' = 'USD' THEN amount * eth_price_dollar
-          WHEN cover_asset = 'ETH'
-          AND '{{display_currency}}' = 'ETH' THEN amount
-          WHEN cover_asset = 'DAI'
-          AND '{{display_currency}}' = 'USD' THEN amount * dai_price_dollar
-          WHEN cover_asset = 'DAI'
-          AND '{{display_currency}}' = 'ETH' THEN amount * dai_price_dollar / eth_price_dollar
-          ELSE 0
-        END
-      ) OVER (
-        ORDER BY
-          claim_time
-      ) AS running_total_claimed
-    from claims_paid
-      inner join prices on claims_paid.claim_time = prices.time
-  )
+total_claims AS (
+  SELECT
+    claim_date,
+    SUM(
+      CASE
+        WHEN cover_asset = 'DAI' AND '{{display_currency}}' = 'USD' then claim_amount * dai_price_dollar
+        WHEN cover_asset = 'DAI' AND '{{display_currency}}' = 'ETH' then claim_amount * dai_price_dollar / eth_price_dollar
+        ELSE 0
+      END
+    ) OVER (ORDER BY claim_date) AS running_dai_claimed,
+    SUM(
+      CASE
+        WHEN cover_asset = 'ETH' AND '{{display_currency}}' = 'USD' then claim_amount * eth_price_dollar
+        WHEN cover_asset = 'ETH' AND '{{display_currency}}' = 'ETH' then claim_amount
+        ELSE 0
+      END
+    ) OVER (ORDER BY claim_date) AS running_eth_claimed,
+    SUM(
+      CASE
+        WHEN cover_asset = 'ETH' AND '{{display_currency}}' = 'USD' then claim_amount * eth_price_dollar
+        WHEN cover_asset = 'ETH' AND '{{display_currency}}' = 'ETH' then claim_amount
+        WHEN cover_asset = 'DAI' AND '{{display_currency}}' = 'USD' then claim_amount * dai_price_dollar
+        WHEN cover_asset = 'DAI' AND '{{display_currency}}' = 'ETH' then claim_amount * dai_price_dollar / eth_price_dollar
+        ELSE 0
+      END
+    ) OVER (ORDER BY claim_date) AS running_total_claimed
+  from claims_paid cp
+    inner join prices p on cp.claim_date = p.block_date and cp.cover_asset = p.symbol
+)
 
 select
   *
 from total_claims
-where claim_time >= timestamp '{{Start Date}}'
-  and claim_time <= timestamp '{{End Date}}'
-order by claim_time desc
+where claim_date >= timestamp '{{Start Date}}'
+  and claim_date <= timestamp '{{End Date}}'
+order by claim_date desc
