30ba0e7604e174cf85fafbe7fdb96d0de5b43825
tomfutago <tomfutago@gmail.com>
2024-07-18 16:36:35 +0100

    more info & split by month

--- COVERS/bd-cover-commissions.sql
+++ COVERS/bd-cover-commissions.sql
@@ -2,14 +2,21 @@ with
 
 commissions as (
   select
+    date_trunc('month', cover_start_date) as cover_month,
     commission_destination,
     sum(commission) as commission
   from query_3788370
-  group by 1
+  where commission > 0
+  group by 1, 2
 )
 
 select
-  coalesce(ens.name, cast(c.commission_destination as varchar)) as commission_destination,
-  commission
+  cover_month,
+  case
+    when c.commission_destination = 0x586b9b2f8010b284a0197f392156f1a7eb5e86e9 then 'Community Fund'
+    else coalesce(ens.name, cast(c.commission_destination as varchar))
+  end as commission_destination,
+  c.commission
 from commissions c
   left join labels.ens on c.commission_destination = ens.address
+order by 1, 2
