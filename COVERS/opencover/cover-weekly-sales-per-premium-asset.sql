76ff0066a3fa7c4d9eff52dc9c19249fabacd392
tomfutago <tomfutago@gmail.com>
2024-08-01 09:37:18 +0100

    fix metric

--- COVERS/opencover/cover-weekly-sales-per-premium-asset.sql
+++ COVERS/opencover/cover-weekly-sales-per-premium-asset.sql
@@ -6,15 +6,14 @@ covers as (
     cover_start_date,
     cover_end_date,
     payment_asset,
-    usd_cover_amount
+    usd_premium_amount
   from query_3950051 -- opencover - quote base
 )
 
 select
   date_trunc('week', cover_start_date) as period_date,
   payment_asset,
-  count(distinct cover_id) as cover_sold,
-  sum(usd_cover_amount) as usd_cover_amount
+  sum(usd_premium_amount) as usd_premium_amount
 from covers
 group by 1, 2
 order by 1, 2
