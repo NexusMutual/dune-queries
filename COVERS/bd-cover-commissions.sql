4626510d589dcdce7b9cb1d0bc9398c7ed5b8863
tomfutago <tomfutago@gmail.com>
2024-08-27 09:54:43 +0100

    switch to prices spell

--- COVERS/bd-cover-commissions.sql
+++ COVERS/bd-cover-commissions.sql
@@ -8,8 +8,8 @@ daily_avg_prices as (
     avg_usdc_usd_price,
     avg_nxm_eth_price,
     avg_nxm_usd_price
-  from query_3789851 -- prices base (fallback) query
-  --from nexusmutual_ethereum.capital_pool_prices
+  --from query_3789851 -- prices base (fallback) query
+  from nexusmutual_ethereum.capital_pool_prices
 ),
 
 commissions_ext as (
