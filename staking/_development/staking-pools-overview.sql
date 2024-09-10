386a4e6c47641719efd7cd86304ffcc55e586eb9
tomfutago <tomfutago@gmail.com>
2024-09-10 15:10:34 +0100

    sync up with just merged spells

--- staking/_development/staking-pools-overview.sql
+++ staking/_development/staking-pools-overview.sql
@@ -12,8 +12,7 @@ staking_pool_products as (
     product_id,
     coalesce(target_weight, initial_weight) as target_weight
   --from query_3859935 -- staking pools base (fallback) query
-  from test_schema.git_dunesql_73bb53b_nexusmutual_ethereum_staking_pools -- temp until PR merged
-  --from nexusmutual_ethereum.staking_pools
+  from nexusmutual_ethereum.staking_pools
 ),
 
 staking_pools as (
@@ -29,8 +28,7 @@ staking_pools as (
     spp.total_weight as leverage,
     spp.product_count
   --from query_3859935 sp -- staking pools base (fallback) query
-  from test_schema.git_dunesql_73bb53b_nexusmutual_ethereum_staking_pools sp -- temp until PR merged
-  --from nexusmutual_ethereum.staking_pools sp
+  from nexusmutual_ethereum.staking_pools sp
     inner join (
       select
         pool_id,
@@ -49,7 +47,7 @@ staked_nxm_per_pool as (
       select
         pool_address,
         sum(total_amount) as total_amount
-      --from query_3619534 -- staking deposit extensions base query -- can't be used bc of "Error: This query has too many stages and is too complex to execute at once"
+      --from query_3619534 -- staking deposit extensions base query
       from nexusmutual_ethereum.staking_deposit_extensions
       where is_active
       group by 1
