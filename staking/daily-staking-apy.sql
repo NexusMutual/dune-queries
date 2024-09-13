5f5c108546576cb576c824228fe4cec37fd57a7b
tomfutago <tomfutago@gmail.com>
2024-09-13 16:26:12 +0100

    daily staking apy init

---
+++ staking/daily-staking-apy.sql
@@ -0,0 +1,19 @@
+with
+
+staking_pool_names as (
+  select pool_id, pool_name
+  from query_3833996 -- staking pool names base (fallback) query
+)
+
+select
+  s.block_date,
+  s.pool_id,
+  spn.pool_name,
+  s.total_staked_nxm,
+  r.reward_total,
+  r.reward_total / nullif(s.total_staked_nxm, 0) * 36500.0 as apy
+from query_4065286 s -- staked nxm base query
+  inner join query_4068272 r -- daily staking rewards base query
+    on s.pool_id = r.pool_id and s.block_date = r.block_date
+  left join staking_pool_names spn on s.pool_id = spn.pool_id
+order by s.pool_id, s.block_date
