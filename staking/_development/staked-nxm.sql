fc64929c6cf9e2f840b40ccb910caffcf7898c29
tomfutago <tomfutago@gmail.com>
2024-09-13 10:07:31 +0100

    stake since 1st staking event, clean up

--- staking/_development/staked-nxm.sql
+++ staking/_development/staked-nxm.sql
@@ -1,42 +1,20 @@
 with
 
-staking_pool_names as (
-  select pool_id, pool_name
-  from query_3833996 -- staking pool names base (fallback) query
-),
-
-staking_pool_products as (
-  select
-    pool_id,
-    pool_address,
-    product_id,
-    coalesce(target_weight, initial_weight) as target_weight
-  --from query_3859935 -- staking pools base (fallback) query
-  from nexusmutual_ethereum.staking_pools
-),
-
 staking_pools as (
   select distinct
     sp.pool_id,
     sp.pool_address,
-    sp.pool_created_time,
-    sp.is_private_pool,
-    sp.manager,
-    sp.initial_pool_fee,
-    sp.current_management_fee,
-    sp.max_management_fee,
-    spp.total_weight as leverage,
-    spp.product_count
-  --from query_3859935 sp -- staking pools base (fallback) query
+    se.first_stake_event_date
+  --from query_3859935 -- staking pools base (fallback) query
   from nexusmutual_ethereum.staking_pools sp
     inner join (
       select
-        pool_id,
-        count(distinct product_id) filter (where target_weight > 0) as product_count,
-        sum(target_weight) as total_weight
-      from staking_pool_products
+        pool_address,
+        cast(min(block_time) as date) as first_stake_event_date
+      from query_3609519 -- staking events
+      --from nexusmutual_ethereum.staking_events
       group by 1
-    ) spp on sp.pool_id = spp.pool_id
+    ) se on sp.pool_address = se.pool_address
 ),
 
 staking_pool_day_sequence as (
@@ -47,7 +25,7 @@ staking_pool_day_sequence as (
   from staking_pools sp
     cross join unnest (
       sequence(
-        cast(date_trunc('day', sp.pool_created_time) as timestamp),
+        cast(date_trunc('day', sp.first_stake_event_date) as timestamp),
         cast(date_trunc('day', now()) as timestamp),
         interval '1' day
       )
@@ -58,44 +36,49 @@ staked_nxm_per_pool as (
   select
     block_date,
     pool_id,
-    sum(total_amount) as total_nxm_staked,
-    dense_rank() over (partition by pool_id order by block_date desc) as rn
+    pool_address,
+    sum(coalesce(total_amount, 0)) as total_staked_nxm,
+    dense_rank() over (partition by pool_id order by block_date desc) as pool_date_rn
   from (
       -- deposits & deposit extensions
       select
         d.block_date,
         d.pool_id,
+        d.pool_address,
         sum(se.total_amount) as total_amount
       from staking_pool_day_sequence d
         left join query_3619534 se -- staking deposit extensions base query
         --left join nexusmutual_ethereum.staking_deposit_extensions se
           on d.pool_address = se.pool_address
-         --and d.block_date = date_trunc('day', se.block_time)
-         --and d.block_date <= se.tranche_expiry_date
          and d.block_date between date_trunc('day', se.block_time) and se.tranche_expiry_date
-      group by 1,2
+         and se.token_tranche_rn = 1
+      group by 1, 2, 3
       union all
       -- withdrawals (& burns?)
       select
         d.block_date,
         d.pool_id,
+        d.pool_address,
         sum(se.amount) as total_amount
       from staking_pool_day_sequence d
         left join query_3609519 se -- staking events
         --left join nexusmutual_ethereum.staking_events se
           on d.pool_address = se.pool_address
-         --and d.block_date = date_trunc('day', se.block_time)
-         --and d.block_date <= se.tranche_expiry_date
          and d.block_date between date_trunc('day', se.block_time) and se.tranche_expiry_date
       where 1=1
         --and flow_type in ('withdraw', 'stake burn')
         and flow_type = 'withdraw' -- burn TBD
-      group by 1,2
+      group by 1, 2, 3
     ) t
-  group by 1, 2
+  group by 1, 2, 3
 )
 
-select * 
+select
+  block_date,
+  pool_id,
+  pool_address,
+  total_staked_nxm,
+  pool_date_rn
 from staked_nxm_per_pool
-where pool_id = 3
-order by 1
+--where pool_date_rn = 1
+--order by pool_id, block_date
