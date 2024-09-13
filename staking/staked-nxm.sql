1fb9709752869436904e5486e94e947cf2b71f20
tomfutago <tomfutago@gmail.com>
2024-09-13 10:32:39 +0100

    re-arrange queries

---
+++ staking/staked-nxm.sql
@@ -0,0 +1,16 @@
+with
+
+staking_pool_names as (
+  select pool_id, pool_name
+  from query_3833996 -- staking pool names base (fallback) query
+)
+
+select
+  s.block_date,
+  s.pool_id,
+  spn.pool_name,
+  s.total_staked_nxm,
+  s.pool_date_rn
+from query_4065286 s
+  left join staking_pool_names spn on s.pool_id = spn.pool_id
+order by s.pool_id, s.block_date
