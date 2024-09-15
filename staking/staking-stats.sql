5bb9f003b040e59227e9a099e3fe0e478162f9e5
tomfutago <tomfutago@gmail.com>
2024-09-15 11:08:19 +0100

    staking stats by currency

--- staking/staking-stats.sql
+++ staking/staking-stats.sql
@@ -1,12 +1,44 @@
 select
   count(*) as pool_count,
-  sum(total_staked_nxm) as total_staked_nxm,
-  sum(total_allocated_nxm) as total_allocated_nxm,
   sum(total_allocated_nxm) / sum(total_staked_nxm) as leverage,
-  sum(reward_current_total_nxm) as reward_current_total_nxm,
-  sum(reward_expected_total_nxm) as reward_expected_total_nxm,
-  sum(pool_manager_commission_nxm) as pool_manager_commission_nxm,
-  sum(pool_distributor_commission_nxm) as pool_distributor_commission_nxm,
-  sum(staker_commission_nxm) as staker_commission_nxm,
-  sum(total_commission_nxm) as total_commission_nxm
-from query_3599009 -- staking pools overview
+  sum(case '{{currency}}'
+        when 'NXM' then total_staked_nxm
+        when 'ETH' then total_staked_nxm_eth
+        when 'USD' then total_staked_nxm_usd
+      end) as total_staked,
+  sum(case '{{currency}}'
+        when 'NXM' then total_allocated_nxm
+        when 'ETH' then total_allocated_nxm_eth
+        when 'USD' then total_allocated_nxm_usd
+      end) as total_allocated,
+  sum(case '{{currency}}'
+        when 'NXM' then reward_current_total_nxm
+        when 'ETH' then reward_current_total_nxm_eth
+        when 'USD' then reward_current_total_nxm_usd
+      end) as reward_current_total,
+  sum(case '{{currency}}'
+        when 'NXM' then reward_expected_total_nxm
+        when 'ETH' then reward_expected_total_nxm_eth
+        when 'USD' then reward_expected_total_nxm_usd
+      end) as reward_expected_total,
+  sum(case '{{currency}}'
+        when 'NXM' then pool_manager_commission_nxm
+        when 'ETH' then pool_manager_commission_nxm_eth
+        when 'USD' then pool_manager_commission_nxm_usd
+      end) as pool_manager_commission,
+  sum(case '{{currency}}'
+        when 'NXM' then pool_distributor_commission_nxm
+        when 'ETH' then pool_distributor_commission_nxm_eth
+        when 'USD' then pool_distributor_commission_nxm_usd
+      end) as pool_distributor_commission,
+  sum(case '{{currency}}'
+        when 'NXM' then staker_commission_nxm
+        when 'ETH' then staker_commission_nxm_eth
+        when 'USD' then staker_commission_nxm_usd
+      end) as staker_commission,
+  sum(case '{{currency}}'
+        when 'NXM' then total_commission_nxm
+        when 'ETH' then total_commission_nxm_eth
+        when 'USD' then total_commission_nxm_usd
+      end) as total_commission
+from query_3599009 -- staking pools overview - base query
