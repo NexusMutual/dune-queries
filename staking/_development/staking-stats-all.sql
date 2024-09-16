b2002e0b29c4e9ee9d7e9ff7c84ef7954cb0eecd
tomfutago <tomfutago@gmail.com>
2024-09-16 10:54:37 +0100

    staking stats for all 3 currencies

---
+++ staking/_development/staking-stats-all.sql
@@ -0,0 +1,20 @@
+select
+  count(*) as pool_count,
+  sum(total_allocated_nxm) / sum(total_staked_nxm) as leverage,
+  sum(total_staked_nxm) as total_staked_nxm,
+  sum(total_staked_nxm_eth) as total_staked_nxm_eth,
+  sum(total_staked_nxm_usd) as total_staked_nxm_usd,
+  sum(total_allocated_nxm) as total_allocated_nxm,
+  sum(total_allocated_nxm_eth) as total_allocated_nxm_eth,
+  sum(total_allocated_nxm_usd) as total_allocated_nxm_usd,
+  sum(reward_current_total_nxm) as reward_current_total_nxm,
+  sum(reward_current_total_nxm_eth) as reward_current_total_nxm_eth,
+  sum(reward_current_total_nxm_usd) as reward_current_total_nxm_usd,
+  sum(reward_expected_total_nxm) as reward_expected_total_nxm,
+  sum(reward_expected_total_nxm_eth) as reward_expected_total_nxm_eth,
+  sum(reward_expected_total_nxm_usd) as reward_expected_total_nxm_usd,
+  sum(pool_manager_commission_nxm) as pool_manager_commission_nxm,
+  sum(pool_distributor_commission_nxm) as pool_distributor_commission_nxm,
+  sum(staker_commission_nxm) as staker_commission_nxm,
+  sum(total_commission_nxm) as total_commission_nxm
+from query_3599009 -- staking pools overview
