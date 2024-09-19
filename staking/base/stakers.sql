84426444005cd391e4164768895b2782018d5dea
tomfutago <tomfutago@gmail.com>
2024-09-19 15:52:43 +0100

    exclude legacy pooled staking address from the list

--- staking/base/stakers.sql
+++ staking/base/stakers.sql
@@ -86,5 +86,6 @@ select
   listagg(cast(pool_id as varchar), ',') within group (order by pool_id) as pools,
   listagg(cast(token_id as varchar), ',') within group (order by token_id) as tokens
 from stakers
+where staker <> '0x84edffa16bb0b9ab1163abb0a13ff0744c11272f' -- legacy pooled staking
 group by 1
 --order by 2 desc
