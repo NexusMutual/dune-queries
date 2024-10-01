5553fd7c928b807a19222d9a50b77ea979be1ab9
tomfutago <tomfutago@gmail.com>
2024-10-01 15:43:41 +0100

    chore: fix comment typo

--- staking/staking-dash/stakers.sql
+++ staking/staking-dash/stakers.sql
@@ -8,7 +8,7 @@ stakers as (
     sum(staked_nxm_usd) as staked_nxm_usd,
     listagg(cast(pool_id as varchar), ',') within group (order by pool_id) as pools,
     listagg(cast(token_id as varchar), ',') within group (order by token_id) as tokens
-  from query_4077503 -- stakers - basers
+  from query_4077503 -- stakers - base
   where staker <> '0x84edffa16bb0b9ab1163abb0a13ff0744c11272f' -- legacy pooled staking v1
   group by 1
 ),
