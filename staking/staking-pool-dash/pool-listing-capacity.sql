0a13b7695f742b7e60dcf6f90956c53d1ca31d5e
tomfutago <tomfutago@gmail.com>
2024-10-02 14:04:54 +0100

    feat: pool listing capacity & stakers stake expiry timeline

--- staking/staking-pool-dash/pool-listing-capacity.sql
+++ staking/staking-pool-dash/pool-listing-capacity.sql
@@ -1,14 +1,25 @@
+with
+
+params as (
+  select cast(split_part('{{pool}}', ' : ', 1) as int) as pool_id
+)
+
 select
-  pool_id,
-  pool_name,
   product_id,
   product_name,
   product_type,
+  target_weight,
+  case '{{currency}}'
+    when 'NXM' then staked_nxm
+    when 'ETH' then staked_nxm_eth
+    when 'USD' then staked_nxm_usd
+  end as staked,
   case '{{currency}}'
     when 'NXM' then max_capacity_nxm
     when 'ETH' then max_capacity_nxm_eth
     when 'USD' then max_capacity_nxm_usd
   end max_capacity,
+  active_cover_count,
   case '{{currency}}'
     when 'NXM' then nxm_cover_amount
     when 'ETH' then eth_cover_amount
@@ -18,8 +29,9 @@ select
     when 'NXM' then available_capacity_nxm
     when 'ETH' then available_capacity_eth
     when 'USD' then available_capacity_usd
-  end available_capacity
+  end available_capacity,
+  available_capacity_nxm / max_capacity_nxm as utilisation
 from query_3991071 -- staking pools products overview (staked per product)
 where (max_capacity_nxm <> 0 or nxm_cover_amount <> 0 or available_capacity_nxm <> 0)
-  and pool_id = {{pool_id}}
+  and cast(pool_id as int) in (select pool_id from params)
 order by product_id
---
+++ staking/staking-pool-dash/pool-stakers-stake-expiry-timeline.sql
@@ -0,0 +1,28 @@
+with
+
+params as (
+  select cast(split_part('{{pool}}', ' : ', 1) as int) as pool_id
+),
+
+stakers as (
+  select
+    staker,
+    sum(staked_nxm) as staked_nxm,
+    sum(staked_nxm_eth) as staked_nxm_eth,
+    sum(staked_nxm_usd) as staked_nxm_usd,
+    max(stake_expiry_date) as stake_expiry_date
+  from query_4077503 -- stakers - base
+  where staker <> '0x84edffa16bb0b9ab1163abb0a13ff0744c11272f' -- legacy pooled staking v1
+    and cast(pool_id as int) in (select pool_id from params)
+  group by 1
+)
+
+select
+  stake_expiry_date,
+  sum(case '{{currency}}'
+    when 'NXM' then staked_nxm
+    when 'ETH' then staked_nxm_eth
+    when 'USD' then staked_nxm_usd
+  end) over (order by stake_expiry_date desc) as staked
+from stakers
+order by 1
