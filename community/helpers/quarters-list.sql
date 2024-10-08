7c630ff55892e7fad697dded08c4aa35102469a0
tomfutago <tomfutago@gmail.com>
2024-10-08 16:16:31 +0100

    feat: weeks & quarters lists

---
+++ community/helpers/quarters-list.sql
@@ -0,0 +1,15 @@
+select
+  case
+    when cast(d.seq_date as timestamp) = date_trunc('quarter', current_date) then 'current quarter'
+    when cast(d.seq_date as timestamp) = date_add('quarter', -1, date_trunc('quarter', current_date)) then 'last quarter'
+    else cast(d.seq_date as varchar)
+  end as period_date
+from (
+    select sequence(
+        date_add('quarter', -20, date_trunc('quarter', current_date)),
+        date_trunc('quarter', current_date),
+        interval '3' month
+      ) as days
+  ) as days_s
+  cross join unnest(days) as d(seq_date)
+order by cast(d.seq_date as timestamp) desc
---
+++ community/helpers/weeks-list.sql
@@ -0,0 +1,15 @@
+select
+  case
+    when cast(d.seq_date as timestamp) = date_trunc('week', current_date) then 'current week'
+    when cast(d.seq_date as timestamp) = date_add('week', -1, date_trunc('week', current_date)) then 'last week'
+    else cast(d.seq_date as varchar)
+  end as period_date
+from (
+    select sequence(
+        date_add('week', -52, date_trunc('week', current_date)),
+        date_trunc('week', current_date),
+        interval '7' day
+      ) as days
+  ) as days_s
+  cross join unnest(days) as d(seq_date)
+order by cast(d.seq_date as timestamp) desc
