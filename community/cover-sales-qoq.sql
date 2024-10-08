09da414e5deea78b8b1a5c52fa302f4220628a64
tomfutago <tomfutago@gmail.com>
2024-10-08 15:17:53 +0100

    feat: cover sales WoW & QoQ

---
+++ community/cover-sales-qoq.sql
@@ -0,0 +1,12 @@
+select
+  date_trunc('quarter', block_date) as period_date,
+  sum(cover_sold) as cover_sold,
+  sum(eth_cover) as eth_cover,
+  sum(usd_cover) as usd_cover,
+  sum(eth_premium) as eth_premium,
+  sum(usd_premium) as usd_premium
+--from query_3889661 -- BD active cover base
+from nexusmutual_ethereum.covers_daily_agg
+where block_date >= date_add('year', -2, current_date)
+group by 1
+order by 1 desc
