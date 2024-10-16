20fb3c2cbf2e583dc32a98c4aec3b9f518d966b7
tomfutago <tomfutago@gmail.com>
2024-10-16 11:45:42 +0100

    chore: additional comm addresses

--- COVERS/cover-bd-dash/bd-cover-commissions.sql
+++ COVERS/cover-bd-dash/bd-cover-commissions.sql
@@ -49,10 +49,17 @@ commissions_agg as (
 select
   c.cover_month,
   case
-    when c.commission_destination = 0x586b9b2f8010b284a0197f392156f1a7eb5e86e9 then 'Community Fund'
+    when c.commission_destination in (
+      0x586b9b2f8010b284a0197f392156f1a7eb5e86e9,
+      0x8e53D04644E9ab0412a8c6bd228C84da7664cFE3
+    ) then 'Community Fund'
     when c.commission_destination = 0x95abc2a62ee543217cf7640b277ba13d056d904a then 'Unity'
     when c.commission_destination = 0xac0734c62b316041d190438d5d3e5d1359614407 then 'Bright Union'
-    when c.commission_destination in (0xe4994082a0e7f38b565e6c5f4afd608de5eddfbb,0x40329f3e27dd3fe228799b4a665f6f104c2ab6b4) then 'OpenCover'
+    when c.commission_destination in (
+      0xe4994082a0e7f38b565e6c5f4afd608de5eddfbb,
+      0x40329f3e27dd3fe228799b4a665f6f104c2ab6b4,
+      0x5f2b6e70aa6a217e9ecd1ed7d0f8f38ce9a348a2
+    ) then 'OpenCover'
     else coalesce(ens.name, cast(c.commission_destination as varchar))
   end as commission_destination,
   c.eth_commission,
