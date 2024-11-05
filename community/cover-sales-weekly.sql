cd45cafcba103c6bfea2eff88bd03e51f9d8f910
tomfutago <tomfutago@gmail.com>
2024-11-05 15:43:09 +0000

    refactor: convert active to regular cover buys

--- community/cover-sales-weekly.sql
+++ community/cover-sales-weekly.sql
@@ -12,14 +12,15 @@ avg_prices as (
 weekly_aggs as (
   select
     date_trunc('week', block_date) as week_date,
-    sum(eth_active_cover) as eth_active_cover,
-    sum(eth_eth_active_cover) as eth_eth_active_cover,
-    sum(dai_eth_active_cover) as dai_eth_active_cover,
-    sum(usdc_eth_active_cover) as usdc_eth_active_cover,
-    sum(usd_active_cover) as usd_active_cover,
-    sum(eth_usd_active_cover) as eth_usd_active_cover,
-    sum(dai_usd_active_cover) as dai_usd_active_cover,
-    sum(usdc_usd_active_cover) as usdc_usd_active_cover
+    sum(cover_sold) as cover_sold,
+    sum(eth_cover) as eth_cover,
+    sum(eth_eth_cover) as eth_eth_cover,
+    sum(dai_eth_cover) as dai_eth_cover,
+    sum(usdc_eth_cover) as usdc_eth_cover,
+    sum(usd_cover) as usd_cover,
+    sum(eth_usd_cover) as eth_usd_cover,
+    sum(dai_usd_cover) as dai_usd_cover,
+    sum(usdc_usd_cover) as usdc_usd_cover
   --from query_3889661 -- BD active cover base
   from nexusmutual_ethereum.covers_daily_agg
   where block_date >= date_add('week', -52, date_trunc('week', current_date))
@@ -29,14 +30,15 @@ weekly_aggs as (
 select
   a.week_date,
   p.avg_eth_usd_price,
-  a.eth_active_cover,
-  a.eth_eth_active_cover,
-  a.dai_eth_active_cover,
-  a.usdc_eth_active_cover,
-  a.usd_active_cover,
-  a.eth_usd_active_cover,
-  a.dai_usd_active_cover,
-  a.usdc_usd_active_cover
+  a.cover_sold,
+  a.eth_cover,
+  a.eth_eth_cover,
+  a.dai_eth_cover,
+  a.usdc_eth_cover,
+  a.usd_cover,
+  a.eth_usd_cover,
+  a.dai_usd_cover,
+  a.usdc_usd_cover
 from weekly_aggs a
   inner join avg_prices p on a.week_date = p.week_date
 order by 1 desc
