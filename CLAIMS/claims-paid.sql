c1fbf1730f6e181aa415ede1b132f9d749bd8b1a
tomfutago <tomfutago@gmail.com>
2024-11-18 10:48:29 +0000

    chore: add cbBTC

--- CLAIMS/claims-paid.sql
+++ CLAIMS/claims-paid.sql
@@ -15,7 +15,10 @@ claims_paid as (
     dai_usd_claim_amount,
     --USDC
     usdc_eth_claim_amount,
-    usdc_usd_claim_amount
+    usdc_usd_claim_amount,
+    --cbBTC
+    cbbtc_eth_claim_amount,
+    cbbtc_usd_claim_amount
   from query_3911051 -- claims paid base (fallback) query
 )
 
@@ -24,10 +27,11 @@ select
   sum(if('{{display_currency}}' = 'USD', eth_usd_claim_amount, eth_eth_claim_amount)) over (order by claim_date) as eth_claim_total,
   sum(if('{{display_currency}}' = 'USD', dai_usd_claim_amount, dai_eth_claim_amount)) over (order by claim_date) as dai_claim_total,
   sum(if('{{display_currency}}' = 'USD', usdc_usd_claim_amount, usdc_eth_claim_amount)) over (order by claim_date) as usdc_claim_total,
+  sum(if('{{display_currency}}' = 'USD', cbbtc_usd_claim_amount, cbbtc_eth_claim_amount)) over (order by claim_date) as cbbtc_claim_total,
   sum(if(
     '{{display_currency}}' = 'USD',
-    eth_usd_claim_amount + dai_usd_claim_amount + usdc_usd_claim_amount,
-    eth_eth_claim_amount + dai_eth_claim_amount + usdc_eth_claim_amount
+    eth_usd_claim_amount + dai_usd_claim_amount + usdc_usd_claim_amount + cbbtc_usd_claim_amount,
+    eth_eth_claim_amount + dai_eth_claim_amount + usdc_eth_claim_amount + cbbtc_eth_claim_amount
   )) over (order by claim_date) as claim_total
 from claims_paid
 where claim_date >= timestamp '{{Start Date}}'
--- CLAIMS/closed-claims-v2.sql
+++ CLAIMS/closed-claims-v2.sql
@@ -85,7 +85,8 @@ prices as (
   where minute > timestamp '2019-05-01'
     and ((symbol = 'ETH' and blockchain is null and contract_address is null)
       or (symbol = 'DAI' and blockchain = 'ethereum' and contract_address = 0x6b175474e89094c44da98b954eedeac495271d0f)
-      or (symbol = 'USDC' and blockchain = 'ethereum' and contract_address = 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48))
+      or (symbol = 'USDC' and blockchain = 'ethereum' and contract_address = 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48)
+      or (symbol = 'cbBTC' and blockchain = 'ethereum' and contract_address = 0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf))
   group by 1, 2
 )
 
