5e10f1c21bed8475f1dfd653316886db67bf7c66
tomfutago <tomfutago@gmail.com>
2024-12-08 13:59:55 +0000

    feat: capital pool totals extract

---
+++ covers/wallet-monitoring/capital-pool-totals.sql
@@ -0,0 +1,8 @@
+select
+  block_date,
+  avg_eth_usd_price,
+  avg_capital_pool_eth_total,
+  avg_capital_pool_usd_total
+from nexusmutual_ethereum.capital_pool_totals
+where block_date >= timestamp '2024-12-01'
+order by 1
