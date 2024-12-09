07fc20a977b835e444a8466b9dcc3a77c22bd239
tomfutago <tomfutago@gmail.com>
2024-12-09 09:30:11 +0000

    wip: nexus cover protocol list

---
+++ covers/wallet-monitoring/protocol-list.sql
@@ -0,0 +1,213 @@
+with protocols(protocol, na) as (
+select '0x v3', null union all
+select '0x v3', null union all
+select '0x v3', null union all
+select '1Inch (DEX & Liquidity Pools)', null union all
+select '1Inch (DEX & Liquidity Pools)', null union all
+select '1Inch (DEX & Liquidity Pools)', null union all
+select 'Aave v2', null union all
+select 'Aave v2', null union all
+select 'Aave v3', null union all
+select 'Aave v3', null union all
+select 'Aerodrome', null union all
+select 'Aerodrome', null union all
+select 'Aevo', null union all
+select 'Ajna Finance', null union all
+select 'AlienBase', 'n/a' union all
+select 'Aloe', 'n/a' union all
+select 'Angle v1', null union all
+select 'Angle v1', null union all
+select 'Arcadia', null union all
+select 'Arrakis', null union all
+select 'Aura v1', null union all
+select 'Aura v1', null union all
+select 'Balancer v2', null union all
+select 'Balancer v2', null union all
+select 'Balancer v2', null union all
+select 'Balancer v3', 'n/a' union all
+select 'BaseSwap', 'n/a' union all
+select 'Beefy v2', null union all
+select 'Beefy v2', null union all
+select 'Beethoven X', null union all
+select 'Beethoven X', null union all
+select 'Camelot', null union all
+select 'Camelot', null union all
+select 'Compound v2', null union all
+select 'Compound v2', null union all
+select 'Compound v3', null union all
+select 'Compound v3', null union all
+select 'Conic Finance', 'n/a' union all
+select 'Contango', null union all
+select 'Convex Finance v1', null union all
+select 'Convex Finance v1', null union all
+select 'CoW AMM', null union all
+select 'CoW AMM', null union all
+select 'CoW AMM', null union all
+select 'CowSwap', 'n/a' union all
+select 'CowSwap', 'n/a' union all
+select 'CowSwap', 'n/a' union all
+select 'Curve All Pools (incl staking)', null union all
+select 'Curve All Pools (incl staking)', null union all
+select 'Curve All Pools (incl staking)', null union all
+select 'Curve LlamaLend', null union all
+select 'DAI Savings Rate (sDAI)', 'n/a' union all
+select 'DAI Savings Rate (sDAI)', 'n/a' union all
+select 'DAI Savings Rate (sDAI)', 'n/a' union all
+select 'DeFi Saver', 'n/a' union all
+select 'Derive (formerly Lyra)', null union all
+select 'Dolomite', null union all
+select 'Eclipse', 'n/a' union all
+select 'EigenLayer v1', null union all
+select 'EigenLayer v1', null union all
+select 'EigenLayer v1', null union all
+select 'Elixir', 'n/a' union all
+select 'Enzyme v4', null union all
+select 'Enzyme v4', null union all
+select 'Equalizer', 'n/a' union all
+select 'Equilibria', null union all
+select 'Ether.fi', null union all
+select 'Ether.fi', null union all
+select 'Ether.fi Liquid', null union all
+select 'Ether.fi Vaults', null union all
+select 'Euler Finance v2', null union all
+select 'Exactly', null union all
+select 'ExtraFi', null union all
+select 'f(x) Protocol', null union all
+select 'f(x) Protocol', null union all
+select 'Flat Money (Sherlock)', null union all
+select 'Flat Money (Sherlock)', null union all
+select 'Fluid', 'n/a' union all
+select 'Fluid', 'n/a' union all
+select 'Frax Finance', 'n/a' union all
+select 'Frax Finance', 'n/a' union all
+select 'Frax Staked ETH', 'n/a' union all
+select 'Fraxlend', null union all
+select 'Fraxswap', 'n/a' union all
+select 'Gains Network', 'n/a' union all
+select 'Gearbox v3', null union all
+select 'GMX v2', null union all
+select 'GMX v2', null union all
+select 'Harvest Finance', 'n/a' union all
+select 'HOT AMM', 'n/a' union all
+select 'Hyperliquid', null union all
+select 'Index Coop', null union all
+select 'Instadapp Lite', null union all
+select 'Inverse Finance FiRM', null union all
+select 'Inverse sDOLA', null union all
+select 'Inverse sDOLA', null union all
+select 'Karak', null union all
+select 'Karak', null union all
+select 'KelpDAO', null union all
+select 'Kiln', null union all
+select 'Kiln', null union all
+select 'Kwenta', null union all
+select 'Kwenta', null union all
+select 'Lido', null union all
+select 'Lido', null union all
+select 'Lido', null union all
+select 'Liquity', null union all
+select 'Liquity', null union all
+select 'Lombard Finance EVM instance', null union all
+select 'Lombard Finance EVM instance', null union all
+select 'MakerDAO', null union all
+select 'Maverick', null union all
+select 'Mellow Finance', null union all
+select 'Moonwell', null union all
+select 'Moonwell', null union all
+select 'Morpho + Morpho Vaults', null union all
+select 'Morpho + Morpho Vaults', null union all
+select 'Mummy', null union all
+select 'Notional v3', null union all
+select 'Notional v3', null union all
+select 'Origin OUSD', null union all
+select 'Origin Protocol', null union all
+select 'Origin Protocol', null union all
+select 'Origin superOETH', null union all
+select 'PancakeSwap', null union all
+select 'PancakeSwap', null union all
+select 'Paraswap', null union all
+select 'Paraswap', null union all
+select 'Paraswap', null union all
+select 'Pendle', null union all
+select 'Pendle', null union all
+select 'Pharaoh', null union all
+select 'PoolTogether v5', null union all
+select 'PoolTogether v5', null union all
+select 'Prisma Finance', null union all
+select 'Prisma Finance', null union all
+select 'Puffer Finance', null union all
+select 'Puffer Finance', null union all
+select 'Quickswap', null union all
+select 'Ramses', null union all
+select 'Reflexer Finance', null union all
+select 'Renzo', null union all
+select 'Renzo', null union all
+select 'Reserve Finance', null union all
+select 'Rocket Pool', null union all
+select 'Rocket Pool', null union all
+select 'Rocket Pool', null union all
+select 'Safe', null union all
+select 'Safe', null union all
+select 'Safe', null union all
+select 'Savings crvUSD (scrvUSD)', null union all
+select 'Savings crvUSD (scrvUSD)', null union all
+select 'Savings crvUSD (scrvUSD)', null union all
+select 'Set Protocol v2', null union all
+select 'Silo', null union all
+select 'Sky Protocol', null union all
+select 'Sky Protocol', null union all
+select 'Sky Savings Rate (sUSDS)', null union all
+select 'Sky Savings Rate (sUSDS)', null union all
+select 'Sky Savings Rate (sUSDS)', null union all
+select 'Spark Protocol', null union all
+select 'Spark Protocol', null union all
+select 'Spectra Finance', null union all
+select 'Stader', null union all
+select 'Stader', null union all
+select 'StakeDAO', null union all
+select 'Stakewise', null union all
+select 'Stakewise', null union all
+select 'Stakewise', null union all
+select 'Sturdy', null union all
+select 'Summerfi', null union all
+select 'Superform', null union all
+select 'SushiSwap', null union all
+select 'Swell', null union all
+select 'Swell', null union all
+select 'Symbiotic', null union all
+select 'Symbiotic', null union all
+select 'Synthetix', null union all
+select 'Synthetix', null union all
+select 'Syrup', null union all
+select 'Syrup', null union all
+select 'Term Finance', null union all
+select 'Term Finance', null union all
+select 'Toros Finance', null union all
+select 'Toros Finance', null union all
+select 'Treehouse', null union all
+select 'Uniswap v2', null union all
+select 'Uniswap v2', null union all
+select 'Uniswap v3', null union all
+select 'Uniswap v3', null union all
+select 'Uniswap v3', null union all
+select 'Usual', null union all
+select 'Veda', null union all
+select 'Velodrome', null union all
+select 'Velodrome', null union all
+select 'Yearn Finance', null union all
+select 'Yearn v2', null union all
+select 'Yearn v2', null union all
+select 'Yearn v3', null
+)
+
+--select distinct plan from plans order by 1
+
+select
+  pr.product_id,
+  coalesce(pr.product_name, 'n/a') as product_name,
+  p.protocol
+from (select /*distinct*/ protocol, na from protocols) p
+  left join nexusmutual_ethereum.products_v2 pr on p.protocol = pr.product_name
+--where pr.product_id is null
+--  and coalesce(p.na, '') <> 'n/a'
+order by lower(p.protocol)
