51e769c1a07741557b0bc40e33d7679fbc501afb
tomfutago <tomfutago@gmail.com>
2024-12-27 13:34:14 +0000

    refactor: ramm swaps below

--- ramm/ramm_swaps_below.sql
+++ ramm/ramm_swaps_below.sql
@@ -1,45 +1,35 @@
-WITH
-  swaps AS (
-    SELECT DISTINCT
-      date_trunc('minute', evt_block_time) AS ts,
-      ethOut,
-      nxmIn,
-      member,
-      CAST(ethOut AS DOUBLE) / CAST(nxmIn AS DOUBLE) AS swap_price
-    FROM
-      nexusmutual_ethereum.Ramm_evt_NxmSwappedForEth
-  ),
-  prices AS (
-    SELECT DISTINCT
-      date_trunc('minute', minute) AS ts,
-      symbol,
-      AVG(price) OVER (
-        PARTITION BY
-          date_trunc('minute', minute)
-      ) AS price_dollar
-    FROM prices.usd
-    WHERE symbol = 'ETH'
-      AND blockchain IS NULL
-      AND minute > CAST('2023-11-11' AS TIMESTAMP)
-  ),
-  swaps_below AS (
-    SELECT
-      a.ts,
-      a.member,
-      a.nxmIn * 1E-18 AS nxmIn,
-      a.ethOut * 1E-18 AS ethOut,
-      swap_price AS swap_price_b,
-      swap_price * price_dollar AS swap_price_b_dollar
-    FROM
-      swaps AS a
-      INNER JOIN prices AS b ON a.ts = b.ts
-  )
-SELECT
-  *
-FROM
-  swaps_below
-WHERE
-  ts >= CAST('{{Start Date}}' AS TIMESTAMP)
-  AND ts <= CAST('{{End Date}}' AS TIMESTAMP)
-ORDER BY
-  ts DESC
+with
+
+prices as (
+  select
+    date_trunc('minute', minute) as block_minute,
+    avg(price) as avg_eth_usd_price
+  from prices.usd
+  where symbol = 'ETH'
+    and blockchain is null
+    and minute > cast('2023-11-11' as timestamp)
+  group by 1
+),
+
+swaps_below as (
+  select
+    date_trunc('minute', evt_block_time) as block_minute,
+    member,
+    nxmIn / 1e18 as nxm_in,
+    ethOut / 1e18 as eth_out,
+    cast(ethOut as double) / cast(nxmIn as double) as nxm_eth_swap_price
+  from nexusmutual_ethereum.Ramm_evt_NxmSwappedForEth
+)
+
+select
+  s.block_minute,
+  s.member,
+  s.nxm_in,
+  s.eth_out,
+  s.nxm_eth_swap_price,
+  s.nxm_eth_swap_price * p.avg_eth_usd_price as nxm_usd_swap_price
+from swaps_below s
+  inner join prices p on s.block_minute = p.block_minute     
+where s.block_minute >= cast('{{Start Date}}' as timestamp)
+  and s.block_minute <= cast('{{End Date}}' as timestamp)
+order by 1 desc
