0261fb6028626d6d0bb86abaf6db4a452808c683
tomfutago <tomfutago@gmail.com>
2025-01-02 10:30:12 +0000

    refactor: nxm supply

--- capital-pool/nxm_supply.sql
+++ capital-pool/nxm_supply.sql
@@ -1,72 +1,32 @@
-WITH
-  minted_nxm AS (
-    SELECT
-      DATE_TRUNC('day', evt_block_time) AS day,
-      SUM(CAST(value AS DOUBLE) / 1e18) AS NXM_supply_minted
-    FROM
-      nexusmutual_ethereum.NXMToken_evt_Transfer AS t
-    WHERE
-      t."from" = 0x0000000000000000000000000000000000000000
-    GROUP BY
-      1
-    ORDER BY
-      day
-  ),
-  burnt_nxm AS (
-    SELECT
-      DATE_TRUNC('day', evt_block_time) AS day,
-      SUM(-1 * CAST(value AS DOUBLE) / 1e18) AS NXM_supply_burnt
-    FROM
-      nexusmutual_ethereum.NXMToken_evt_Transfer AS t
-    WHERE
-      t."to" = 0x0000000000000000000000000000000000000000
-    GROUP BY
-      1
-    ORDER BY
-      day
-  ),
-  minted_burnt_nxm AS (
-    SELECT
-      CASE
-        WHEN minted_nxm.day IS NULL THEN burnt_nxm.day
-        ELSE minted_nxm.day
-      END AS day,
-      CASE
-        WHEN NXM_supply_minted IS NULL THEN 0
-        ELSE NXM_supply_minted
-      END AS NXM_supply_minted,
-      CASE
-        WHEN NXM_supply_burnt IS NULL THEN 0
-        ELSE NXM_supply_burnt
-      END AS NXM_supply_burnt
-    FROM
-      minted_nxm
-      FULL JOIN burnt_nxm ON minted_nxm.day = burnt_nxm.day
-  ),
-  nxm_supply AS (
-    SELECT
-      day,
-      SUM(NXM_supply_minted) OVER (
-        ORDER BY
-          day
-      ) AS total_nxm_minted,
-      SUM(NXM_supply_burnt) OVER (
-        ORDER BY
-          day
-      ) AS total_nxm_burned,
-      SUM(NXM_supply_minted + NXM_supply_burnt) OVER (
-        ORDER BY
-          day
-      ) AS total_nxm
-    FROM
-      minted_burnt_nxm
-  )
-SELECT
-  *
-FROM
-  nxm_supply
-WHERE
-  day >= CAST('{{Start Date}}' AS TIMESTAMP)
-  AND day <= CAST('{{End Date}}' AS TIMESTAMP)
-ORDER BY
-  day DESC NULLS FIRST
+with
+
+nxm_transfer as (
+  select
+    date_trunc('day', evt_block_time) as block_date,
+    if("from" = 0x0000000000000000000000000000000000000000, 'mint', 'burn') as transfer_type,
+    if("from" = 0x0000000000000000000000000000000000000000, 1, -1) * (value / 1e18) as amount
+  from nexusmutual_ethereum.NXMToken_evt_Transfer
+  where "from" = 0x0000000000000000000000000000000000000000
+    or "to" = 0x0000000000000000000000000000000000000000
+),
+
+nxm_supply as (
+  select
+    block_date,
+    coalesce(sum(amount) filter (where transfer_type = 'mint'), 0) as nxm_mint,
+    coalesce(sum(amount) filter (where transfer_type = 'burn'), 0) as nxm_burn
+  from nxm_transfer
+  group by 1
+)
+
+select
+  block_date,
+  nxm_mint,
+  nxm_burn,
+  sum(nxm_mint) over (order by block_date) as total_nxm_mint,
+  sum(nxm_burn) over (order by block_date) as total_nxm_burn,
+  sum(nxm_mint + nxm_burn) over (order by block_date) as total_nxm
+from nxm_supply
+where block_date >= cast('{{Start Date}}' as timestamp)
+  and block_date <= cast('{{End Date}}' as timestamp)
+order by 1 desc
