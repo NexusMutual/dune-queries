a035c25c25029f78e0fedada365de2c5dde69890
tomfutago <tomfutago@gmail.com>
2025-01-03 14:16:01 +0000

    feat: add redemption_pct calc

--- ramm/ramm_swaps_below.sql
+++ ramm/ramm_swaps_below.sql
@@ -21,18 +21,33 @@ swaps_below as (
   from query_4498669 -- RAMM swaps - base
   where swap_type = 'below'
   group by 1
+),
+
+swaps_x_bv as (
+  select
+    bv.block_date,
+    s.nxm_in,
+    s.eth_out,
+    s.nxm_eth_swap_price,
+    s.nxm_eth_swap_price * bv.avg_eth_usd_price as nxm_usd_swap_price,
+    bv.eth_book_value,
+    bv.usd_book_value,
+    if('{{currency}}' = 'USD', s.nxm_eth_swap_price * bv.avg_eth_usd_price, s.nxm_eth_swap_price) as swap_price,
+    if('{{currency}}' = 'USD', bv.usd_book_value, bv.eth_book_value) as book_value
+  from book_value bv
+    inner join swaps_below s on bv.block_date = s.block_date
 )
 
 select
-  bv.block_date,
-  s.nxm_in,
-  s.eth_out,
-  s.nxm_eth_swap_price,
-  s.nxm_eth_swap_price * bv.avg_eth_usd_price as nxm_usd_swap_price,
-  bv.eth_book_value,
-  bv.usd_book_value,
-  if('{{currency}}' = 'USD', s.nxm_eth_swap_price * bv.avg_eth_usd_price, s.nxm_eth_swap_price) as swap_price,
-  if('{{currency}}' = 'USD', bv.usd_book_value, bv.eth_book_value) as book_value
-from book_value bv
-  inner join swaps_below s on bv.block_date = s.block_date
+  block_date,
+  nxm_in,
+  eth_out,
+  nxm_eth_swap_price,
+  nxm_usd_swap_price,
+  eth_book_value,
+  usd_book_value,
+  swap_price,
+  book_value,
+  swap_price / book_value * 100 as redemption_pct
+from swaps_x_bv
 order by 1 desc
