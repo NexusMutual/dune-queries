0647aeaf77d54b376c35d61d325c7bc597f81e0c
tomfutago <tomfutago@gmail.com>
2025-01-03 09:53:36 +0000

    refactor: add nxm supply base & simplify deps

--- capital-pool/book_value.sql
+++ capital-pool/book_value.sql
@@ -1,54 +1,21 @@
 with
 
 capital_pool as (
-  select *
-  --from query_3773633 -- Capital Pool base (fallback) query
-  from nexusmutual_ethereum.capital_pool_totals
-),
-
-display_currency_total as (
   select
     block_date,
     if('{{display_currency}}' = 'USD', avg_capital_pool_usd_total, avg_capital_pool_eth_total) as capital_pool_display_curr
-  from capital_pool
-),
-
-nxm_events as (
-  select
-    block_date,
-    sum(nxm_amount) as nxm_amount
-  from (
-    select
-      date_trunc('day', evt_block_time) as block_date,
-      sum(cast(value as double) / 1e18) as nxm_amount
-    from nexusmutual_ethereum.NXMToken_evt_Transfer
-    where "from" = 0x0000000000000000000000000000000000000000
-    group by 1
-    union all
-    select
-      date_trunc('day', evt_block_time) as block_date,
-      -1 * sum(cast(value as double) / 1e18) as nxm_amount
-    from nexusmutual_ethereum.NXMToken_evt_Transfer
-    where "to" = 0x0000000000000000000000000000000000000000
-    group by 1
-  ) t
-  group by 1
-),
-
-nxm_supply as (
-  select
-    block_date,
-    sum(nxm_amount) over (order by block_date) as nxm_total
-  from nxm_events
+  --from query_3773633 -- Capital Pool base (fallback) query
+  from nexusmutual_ethereum.capital_pool_totals
 )
 
 select
-  ct.block_date,
-  ct.capital_pool_display_curr,
-  ns.nxm_total,
-  ct.capital_pool_display_curr / ns.nxm_total as book_value
-from display_currency_total ct
-  inner join nxm_supply ns on ct.block_date = ns.block_date
-where ct.block_date >= timestamp '{{Start Date}}'
-  and ct.block_date < timestamp '{{End Date}}'
-order by block_date desc nulls first
+  cp.block_date,
+  cp.capital_pool_display_curr,
+  ns.total_nxm as nxm_total,
+  cp.capital_pool_display_curr / ns.total_nxm as book_value
+from capital_pool cp
+  inner join query_4514857 ns -- NXM supply base
+    on cp.block_date = ns.block_date
+where cp.block_date >= timestamp '{{Start Date}}'
+  and cp.block_date < timestamp '{{End Date}}'
+order by 1 desc
--- capital-pool/nxm_supply.sql
+++ capital-pool/nxm_supply.sql
@@ -1,32 +1,11 @@
-with
-
-nxm_transfer as (
-  select
-    date_trunc('day', evt_block_time) as block_date,
-    if("from" = 0x0000000000000000000000000000000000000000, 'mint', 'burn') as transfer_type,
-    if("from" = 0x0000000000000000000000000000000000000000, 1, -1) * (value / 1e18) as amount
-  from nexusmutual_ethereum.NXMToken_evt_Transfer
-  where "from" = 0x0000000000000000000000000000000000000000
-    or "to" = 0x0000000000000000000000000000000000000000
-),
-
-nxm_supply as (
-  select
-    block_date,
-    coalesce(sum(amount) filter (where transfer_type = 'mint'), 0) as nxm_mint,
-    coalesce(sum(amount) filter (where transfer_type = 'burn'), 0) as nxm_burn
-  from nxm_transfer
-  group by 1
-)
-
 select
   block_date,
   nxm_mint,
   nxm_burn,
-  sum(nxm_mint) over (order by block_date) as total_nxm_mint,
-  sum(nxm_burn) over (order by block_date) as total_nxm_burn,
-  sum(nxm_mint + nxm_burn) over (order by block_date) as total_nxm
-from nxm_supply
+  total_nxm_mint,
+  total_nxm_burn,
+  total_nxm
+from query_4514857 -- NXM supply base
 where block_date >= cast('{{Start Date}}' as timestamp)
   and block_date <= cast('{{End Date}}' as timestamp)
 order by 1 desc
