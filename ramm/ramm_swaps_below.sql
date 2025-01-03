2291f3f8a2aa561079486c4b44189682722063ac
tomfutago <tomfutago@gmail.com>
2025-01-03 10:48:03 +0000

    feat: add book value

--- ramm/ramm_swaps_below.sql
+++ ramm/ramm_swaps_below.sql
@@ -1,36 +1,38 @@
 with
 
-prices as (
+book_value as (
   select
-    date_trunc('minute', minute) as block_minute,
-    avg(price) as avg_eth_usd_price
-  from prices.usd
-  where symbol = 'ETH'
-    and blockchain is null
-    and minute > cast('2023-11-11' as timestamp)
-  group by 1
+    cp.block_date,
+    cp.avg_eth_usd_price,
+    cp.avg_capital_pool_usd_total / ns.total_nxm as usd_book_value,
+    cp.avg_capital_pool_eth_total / ns.total_nxm as eth_book_value
+  --from query_3773633 cp -- Capital Pool base (fallback) query
+  from nexusmutual_ethereum.capital_pool_totals cp
+    inner join query_4514857 ns -- NXM supply base
+      on cp.block_date = ns.block_date
 ),
 
 swaps_below as (
   select
-    date_trunc('minute', block_time) as block_minute,
-    member,
-    amount_in as nxm_in,
-    amount_out as eth_out,
-    swap_price as nxm_eth_swap_price
+    date_trunc('day', block_time) as block_date,
+    sum(amount_in) as nxm_in,
+    sum(amount_out) as eth_out,
+    avg(swap_price) as nxm_eth_swap_price
   from query_4498669 -- RAMM swaps - base
   where swap_type = 'below'
+  group by 1
 )
 
 select
-  s.block_minute,
-  s.member,
+  bv.block_date,
   s.nxm_in,
   s.eth_out,
   s.nxm_eth_swap_price,
-  s.nxm_eth_swap_price * p.avg_eth_usd_price as nxm_usd_swap_price
-from swaps_below s
-  inner join prices p on s.block_minute = p.block_minute     
-where s.block_minute >= cast('{{Start Date}}' as timestamp)
-  and s.block_minute <= cast('{{End Date}}' as timestamp)
+  s.nxm_eth_swap_price * bv.avg_eth_usd_price as nxm_usd_swap_price,
+  bv.eth_book_value,
+  bv.usd_book_value,
+  if('{{currency}}' = 'USD', s.nxm_eth_swap_price * bv.avg_eth_usd_price, s.nxm_eth_swap_price) as swap_price,
+  if('{{currency}}' = 'USD', bv.usd_book_value, bv.eth_book_value) as book_value
+from book_value bv
+  inner join swaps_below s on bv.block_date = s.block_date
 order by 1 desc
