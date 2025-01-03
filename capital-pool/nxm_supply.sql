7be8e2d2f9433433e1af4a8b0ccb25301f42a802
tomfutago <tomfutago@gmail.com>
2025-01-03 16:15:10 +0000

    chore: remove date params

--- capital-pool/nxm_supply.sql
+++ capital-pool/nxm_supply.sql
@@ -6,6 +6,4 @@ select
   total_nxm_burn,
   total_nxm
 from query_4514857 -- NXM supply base
-where block_date >= cast('{{Start Date}}' as timestamp)
-  and block_date <= cast('{{End Date}}' as timestamp)
 order by 1 desc
