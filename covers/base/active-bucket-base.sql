65292bdc7b508f1edb98c9faf244b8419c911f7d
tomfutago <tomfutago@gmail.com>
2025-02-13 10:28:34 +0000

    chore: update comment

--- covers/base/active-bucket-base.sql
+++ covers/base/active-bucket-base.sql
@@ -12,4 +12,4 @@ select
   partial_cover_amount as nxm_cover_amount_per_pool,
   sum(partial_cover_amount) over (partition by cover_id) as nxm_cover_amount_total
 from query_4599092 -- covers v2 - base root (fallback query)
-where cover_end_time >= from_unixtime(28.0 * 86400.0 * cast(floor(date_diff('day', from_unixtime(0), now()) / 28.0) as double)) -- bucket_expiry_date
+where cover_end_time >= from_unixtime(28.0 * 86400.0 * cast(floor(date_diff('day', from_unixtime(0), now()) / 28.0) as double)) -- bucket start date
