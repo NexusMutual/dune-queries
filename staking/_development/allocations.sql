6c884ffc050158ee1fe512e34006aee5dc3a9595
tomfutago <tomfutago@gmail.com>
2025-02-13 13:55:55 +0000

    wip: testing allocations

--- staking/_development/allocations.sql
+++ staking/_development/allocations.sql
@@ -2,7 +2,7 @@ with
 
 request_allocations as (
   select
-    call_block_time as block_time,
+    call_block_time as allocation_time,
     call_block_number as block_number,
     output_allocationId as allocation_id,
     output_premium as premium,
@@ -15,6 +15,9 @@ request_allocations as (
     cast(json_query(request, 'lax $.rewardRatio') as bigint) as reward_ratio,
     cast(json_query(request, 'lax $.globalMinPrice') as bigint) as global_min_price,
     cast(json_query(request, 'lax $.globalCapacityRatio') as bigint) as global_capacity_ratio,
+    cast(json_query(request, 'lax $.capacityReductionRatio') as bigint) as capacity_reduction_ratio,
+    cast(json_query(request, 'lax $.previousStart') as bigint) as previous_start,
+    cast(json_query(request, 'lax $.previousExpiration') as bigint) as previous_expiration,
     call_trace_address,
     call_tx_hash as tx_hash,
     row_number() over (partition by call_block_number, call_tx_hash, output_allocationId order by call_trace_address desc) as rn
@@ -22,16 +25,18 @@ request_allocations as (
   where call_success
 ),
 
-covers as (
+covers_per_active_bucket as (
   select
     cover_id,
     cover_start_time,
     cover_end_time,
     staking_pool_id,
-    product_id
-  from nexusmutual_ethereum.covers_v2
-  --from query_3788370 -- covers v2 base (fallback) query
-  where cover_end_date >= current_date
+    product_id,
+    cover_asset,
+    sum_assured,
+    partial_cover_amount as nxm_cover_amount_per_pool
+  from query_4599092 -- covers v2 - base root (fallback query)
+  where cover_end_time >= from_unixtime(28.0 * 86400.0 * cast(floor(date_diff('day', from_unixtime(0), now()) / 28.0) as double)) -- bucket start date
 ),
 
 allocations as (
@@ -40,21 +45,57 @@ allocations as (
     c.cover_id,
     c.product_id,
     a.allocation_id,
-    a.block_time,
-    a.period,
-    a.grace_period,
+    a.allocation_time,
+    a.period / 86400.0 as period,
+    a.grace_period / 86400.0 as grace_period,
+    a.use_fixed_price,
+    a.reward_ratio,
+    a.global_min_price,
+    a.global_capacity_ratio,
+    a.capacity_reduction_ratio,
+    a.previous_start,
+    a.previous_expiration,
+    c.cover_asset,
+    c.sum_assured,
+    c.nxm_cover_amount_per_pool,
     a.amount / 1e18 as amount,
     a.premium / 1e18 as premium
   from request_allocations a
-    inner join covers c on a.cover_id = c.cover_id and a.product_id = c.product_id
+    inner join covers_per_active_bucket c on a.cover_id = c.cover_id and a.product_id = c.product_id
   where a.rn = 1
-    and a.cover_id > 0
+    --and a.cover_id > 0
 )
 
 select
   staking_pool_id,
+  product_id,
+  --period,
+  --grace_period,
+  --use_fixed_price,
+  --reward_ratio,
+  --global_min_price,
+  --global_capacity_ratio,
+  --capacity_reduction_ratio,
+  cover_id,
+  cover_asset,
+  sum_assured,
+  nxm_cover_amount_per_pool,
+  allocation_id,
+  allocation_time,
+  amount,
+  premium
+from allocations
+where staking_pool_id = 2
+  and product_id = 130
+order by cover_id, allocation_time
+
+/*
+select
+  staking_pool_id,
+  product_id,
   sum(amount) as amount,
   sum(premium) as premium
 from allocations
-group by 1
+group by 1, 2
 order by 1
+*/
