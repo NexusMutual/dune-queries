43117288bcbdda76164bd6a851fad37ed5ac8e5a
tomfutago <tomfutago@gmail.com>
2025-03-09 10:51:38 +0000

    wip: split aave positions per tx type

--- financials/_development/aave.sql
+++ financials/_development/aave.sql
@@ -28,23 +28,31 @@ where block_time >= timestamp '2024-05-23'
 
 with
 
+prices as (
+  select
+    block_date,
+    avg_eth_usd_price,
+    avg_usdc_usd_price
+  from query_4627588 -- Capital Pool - base root
+),
+
 capital_pool as (
   select
     date_trunc('month', block_date) as block_month,
     capital_pool,
-    lead(capital_pool, 1) over (order by block_date desc) as capital_pool_prev,
+    lag(capital_pool, 1) over (order by block_date) as capital_pool_prev,
     eth,
-    lead(eth, 1) over (order by block_date desc) as eth_prev,
+    lag(eth, 1) over (order by block_date) as eth_prev,
     steth,
-    lead(steth, 1) over (order by block_date desc) as steth_prev,
+    lag(steth, 1) over (order by block_date) as steth_prev,
     reth,
-    lead(reth, 1) over (order by block_date desc) as reth_prev,
+    lag(reth, 1) over (order by block_date) as reth_prev,
     nxmty,
-    lead(nxmty, 1) over (order by block_date desc) as nxmty_prev,
+    lag(nxmty, 1) over (order by block_date) as nxmty_prev,
     aweth,
-    lead(aweth, 1) over (order by block_date desc) as aweth_prev,
+    lag(aweth, 1) over (order by block_date) as aweth_prev,
     debt_usdc,
-    lead(debt_usdc, 1) over (order by block_date desc) as debt_usdc_prev
+    lag(debt_usdc, 1) over (order by block_date) as debt_usdc_prev
   from (
     select
       block_date,
@@ -63,65 +71,51 @@ capital_pool as (
   limit 12 -- 12 months rolling
 ),
 
-prices as (
-  select
-    block_date,
-    avg_eth_usd_price,
-    avg_usdc_usd_price
-  from query_4627588 -- Capital Pool - base root
-),
-
-aweth_withdrawals as (
+aweth_collateral as (
   select
     date_trunc('month', block_time) as block_month,
-    sum(amount) as aweth_eth,
-    sum(amount_usd) as aweth_usd
+    sum(if(transaction_type = 'deposit', amount)) as aweth_deposit_eth,
+    sum(if(transaction_type = 'deposit', amount_usd)) as aweth_deposit_usd,
+    sum(if(transaction_type = 'withdraw', amount)) as aweth_withdraw_eth,
+    sum(if(transaction_type = 'withdraw', amount_usd)) as aweth_withdraw_usd
   from aave_ethereum.supply
   where block_time >= timestamp '2024-05-23'
     and coalesce(on_behalf_of, depositor) = 0x51ad1265C8702c9e96Ea61Fe4088C2e22eD4418e -- Advisory Board multisig
     and version = '3'
-    and transaction_type = 'withdraw'
     and symbol = 'WETH'
   group by 1
 ),
 
-usdc_debt_repayments as (
+usdc_debt as (
   select
     date_trunc('month', b.block_time) as block_month,
-    -1 * sum(b.amount / p.avg_eth_usd_price) as usdc_debt_eth,
-    -1 * sum(b.amount_usd) as usdc_debt_usd
+    sum(if(b.transaction_type = 'borrow', b.amount) / p.avg_eth_usd_price) as usdc_debt_borrow_eth,
+    sum(if(b.transaction_type = 'borrow', b.amount_usd)) as usdc_debt_borrow_usd,
+    -1 * sum(if(b.transaction_type = 'repay', b.amount) / p.avg_eth_usd_price) as usdc_debt_repay_eth,
+    -1 * sum(if(b.transaction_type = 'repay', b.amount_usd)) as usdc_debt_repay_usd
   from aave_ethereum.borrow b
     inner join prices p on date_trunc('day', b.block_time) = p.block_date
   where b.block_time >= timestamp '2024-05-23'
     and coalesce(b.on_behalf_of, b.borrower) = 0x51ad1265C8702c9e96Ea61Fe4088C2e22eD4418e -- Advisory Board multisig
     and b.version = '3'
-    and b.transaction_type = 'repay'
     and b.symbol = 'USDC'
   group by 1
 ),
 
-capital_pool_aave_adjustments as (
+capital_pool_enriched as (
   select
     cp.block_month,
-    cp.capital_pool,
-    cp.capital_pool_prev,
-    cp.eth,
-    cp.eth_prev,
-    cp.steth,
-    cp.steth_prev,
-    cp.reth,
-    cp.reth_prev,
-    cp.nxmty,
-    cp.nxmty_prev,
     cp.aweth,
     cp.aweth_prev,
-    w.aweth_eth as aweth_withdrawal,
+    aave_c.aweth_deposit_eth,
+    aave_c.aweth_withdraw_eth,
     cp.debt_usdc,
     cp.debt_usdc_prev,
-    r.usdc_debt_eth as usdc_debt_repayment
+    aave_d.usdc_debt_borrow_eth,
+    aave_d.usdc_debt_repay_eth
   from capital_pool cp
-    left join aweth_withdrawals w on cp.block_month = w.block_month
-    left join usdc_debt_repayments r on cp.block_month = r.block_month
+    left join aweth_collateral aave_c on cp.block_month = aave_c.block_month
+    left join usdc_debt aave_d on cp.block_month = aave_d.block_month
 )
 
-select * from capital_pool_aave_adjustments order by 1 desc
+select * from capital_pool_enriched order by 1 desc
