5978e6fa541faceef6f2256d4a40e67af7949cfa
tomfutago <tomfutago@gmail.com>
2025-03-11 10:20:57 +0000

    fix: claims_paid_count

--- claims/api-endpoints/claims-paid.sql
+++ claims/api-endpoints/claims-paid.sql
@@ -1,5 +1,5 @@
 select
-  count(distinct claim_id) as claims_paid_count,
+  count(distinct row(version, claim_id)) as claims_paid_count,
   sum(eth_usd_claim_amount + dai_usd_claim_amount + usdc_usd_claim_amount + cbbtc_usd_claim_amount) as claims_paid_total_usd
 --from query_3911051 -- claims paid base (fallback query)
 from nexusmutual_ethereum.claims_paid
