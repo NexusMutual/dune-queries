3d149bb6d4989945560a112af382a12f0b7b0d72
tomfutago <tomfutago@gmail.com>
2025-03-12 13:43:38 +0000

    feat: add balance sheet figures

--- financials/fin-statement.sql
+++ financials/fin-statement.sql
@@ -43,6 +43,35 @@ ramm_volume as (
   where block_month = timestamp '2025-02-01'
 ),
 
+balance_sheet as (
+  select
+    block_month,
+    eth_capital_pool,
+    eth_eth,
+    eth_steth,
+    eth_reth,
+    eth_nxmty,
+    eth_aweth,
+    eth_debt_usdc,
+    eth_dai,
+    eth_usdc,
+    eth_cbbtc,
+    eth_cover_re,
+    usd_capital_pool,
+    usd_eth,
+    usd_steth,
+    usd_reth,
+    usd_nxmty,
+    usd_aweth,
+    usd_debt_usdc,
+    usd_dai,
+    usd_usdc,
+    usd_cbbtc,
+    usd_cover_re
+  from query_4841979 -- balance sheet
+  where block_month = timestamp '2025-02-01'
+),
+
 fin_combined as (
   select
     ir.eth_inv_returns,
@@ -54,40 +83,64 @@ fin_combined as (
     rv.eth_nxm_volume,
     rv.usd_nxm_volume,
     rv.eth_eth_volume,
-    rv.usd_eth_volume
+    rv.usd_eth_volume,
+    bs.eth_capital_pool,
+    bs.eth_eth,
+    bs.eth_steth,
+    bs.eth_reth,
+    bs.eth_nxmty,
+    bs.eth_aweth,
+    bs.eth_debt_usdc,
+    bs.eth_dai,
+    bs.eth_usdc,
+    bs.eth_cbbtc,
+    bs.eth_cover_re,
+    bs.usd_capital_pool,
+    bs.usd_eth,
+    bs.usd_steth,
+    bs.usd_reth,
+    bs.usd_nxmty,
+    bs.usd_aweth,
+    bs.usd_debt_usdc,
+    bs.usd_dai,
+    bs.usd_usdc,
+    bs.usd_cbbtc,
+    bs.usd_cover_re
   from investment_returns ir
     cross join premiums_claims pc
     cross join member_fees mf
     cross join ramm_volume rv
+    cross join balance_sheet bs
 )
 
 select
   i.label_tab,
   case i.label
-    when 'Revenue Statement' then null
+    -- revenue
+    when 'Revenue Statement' then eth_inv_returns + fx_change + eth_premiums_minus_claims + eth_member_fee + eth_nxm_volume - eth_eth_volume
     when 'Cash Surplus' then eth_inv_returns + fx_change + eth_premiums_minus_claims + eth_member_fee
     when 'Investments' then eth_inv_returns + fx_change
     when 'Investment Returns' then eth_inv_returns
     when 'Stablecoin Impact' then fx_change
     when 'Premiums - Claims' then eth_premiums_minus_claims
     when 'Membership Fees' then eth_member_fee
-    when 'Reserve Movement' then null
     when 'Capital Movement' then eth_nxm_volume - eth_eth_volume
     when 'Contributions' then eth_nxm_volume
-    when 'Withdrawals' then eth_eth_volume
-    when 'Balance Sheet' then null
-    when 'ETH Denominated Assets' then null
-    when 'ETH' then null
-    when 'stETH' then null
-    when 'rETH' then null
-    when 'cbBTC' then null
-    when 'Enzyme Vault' then null
-    when 'Aave aEthWETH' then null
-    when 'Stablecoin Denominated Assets' then null
-    when 'DAI' then null
-    when 'USDC' then null
-    when 'Cover Re' then null
-    when 'Aave debtUSDC' then null
+    when 'Withdrawals' then -1 * eth_eth_volume
+    -- balance sheet
+    when 'Balance Sheet' then eth_capital_pool
+    when 'ETH Denominated Assets' then eth_eth + eth_steth + eth_reth + eth_cbbtc + eth_nxmty + eth_aweth
+    when 'ETH' then eth_eth
+    when 'stETH' then eth_steth
+    when 'rETH' then eth_reth
+    when 'cbBTC' then eth_cbbtc
+    when 'Enzyme Vault' then eth_nxmty
+    when 'Aave aEthWETH' then eth_aweth
+    when 'Stablecoin Denominated Assets' then eth_dai + eth_usdc + eth_cover_re + eth_debt_usdc
+    when 'DAI' then eth_dai
+    when 'USDC' then eth_usdc
+    when 'Cover Re' then eth_cover_re
+    when 'Aave debtUSDC' then eth_debt_usdc
   end as eth_val,
   null as usd_val
 from fin_combined fc
