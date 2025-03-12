93c9e188e792b875f7f4b50098b0028dd8bef553
tomfutago <tomfutago@gmail.com>
2025-03-12 10:24:36 +0000

    refactor: replace select union with cross join

--- financials/fin-statement.sql
+++ financials/fin-statement.sql
@@ -9,7 +9,6 @@ investment_returns as (
   select
     block_month,
     eth_inv_returns,
-    eth_inv_apy,
     fx_change
   from query_4770697 -- investement returns
   where block_month = timestamp '2025-02-01'
@@ -33,23 +32,27 @@ member_fees as (
   where block_month = timestamp '2025-02-01'
 ),
 
-cash_surplus (label, eth_val, apy) as (
+fin_combined as (
   select
-    'Cash Surplus',
-    (select eth_inv_returns + fx_change from investment_returns)
-    + (select eth_premiums_minus_claims from premiums_claims)
-    + (select eth_member_fee from member_fees),
-    null
-  union all 
-  select 'Investments', eth_inv_returns + fx_change, null from investment_returns union all
-  select 'Investment Returns', eth_inv_returns, eth_inv_apy from investment_returns union all
-  select 'Stablecoin Impact', fx_change, null from investment_returns union all
-  select 'Premiums - Claims', eth_premiums_minus_claims, null from premiums_claims union all
-  select 'Membership Fees', eth_member_fee, null from member_fees
+    ir.eth_inv_returns,
+    ir.fx_change,
+    pc.eth_premiums_minus_claims,
+    pc.usd_premiums_minus_claims,
+    mf.eth_member_fee,
+    mf.usd_member_fee
+  from investment_returns ir, premiums_claims pc, member_fees mf
 )
 
 select
-  i.label_tab, cs.eth_val, cs.apy
-from items i
-  left join cash_surplus cs on i.label = cs.label
+  i.label_tab,
+  case i.label
+    when 'Cash Surplus' then eth_inv_returns + fx_change + eth_premiums_minus_claims + eth_member_fee
+    when 'Investments' then eth_inv_returns + fx_change
+    when 'Investment Returns' then eth_inv_returns
+    when 'Stablecoin Impact' then fx_change
+    when 'Premiums - Claims' then eth_premiums_minus_claims
+    when 'Membership Fees' then eth_member_fee
+  end as eth_val
+from fin_combined fc
+  cross join items i
 order by i.fi_id
