f963da86527a416d1a2c5215d43f0488080cc429
tomfutago <tomfutago@gmail.com>
2025-03-12 11:57:20 +0000

    feat: expand statemnt list + add ramm volumes

--- financials/fin-statement.sql
+++ financials/fin-statement.sql
@@ -32,6 +32,17 @@ member_fees as (
   where block_month = timestamp '2025-02-01'
 ),
 
+ramm_volume as (
+  select
+    block_month,
+    eth_nxm_volume,
+    usd_nxm_volume,
+    eth_eth_volume,
+    usd_eth_volume
+  from query_4841361 -- ramm volume
+  where block_month = timestamp '2025-02-01'
+),
+
 fin_combined as (
   select
     ir.eth_inv_returns,
@@ -39,20 +50,46 @@ fin_combined as (
     pc.eth_premiums_minus_claims,
     pc.usd_premiums_minus_claims,
     mf.eth_member_fee,
-    mf.usd_member_fee
-  from investment_returns ir, premiums_claims pc, member_fees mf
+    mf.usd_member_fee,
+    rv.eth_nxm_volume,
+    rv.usd_nxm_volume,
+    rv.eth_eth_volume,
+    rv.usd_eth_volume
+  from investment_returns ir
+    cross join premiums_claims pc
+    cross join member_fees mf
+    cross join ramm_volume rv
 )
 
 select
   i.label_tab,
   case i.label
+    when 'Revenue Statement' then null
     when 'Cash Surplus' then eth_inv_returns + fx_change + eth_premiums_minus_claims + eth_member_fee
     when 'Investments' then eth_inv_returns + fx_change
     when 'Investment Returns' then eth_inv_returns
     when 'Stablecoin Impact' then fx_change
     when 'Premiums - Claims' then eth_premiums_minus_claims
     when 'Membership Fees' then eth_member_fee
-  end as eth_val
+    when 'Reserve Movement' then null
+    when 'Capital Movement' then eth_nxm_volume - eth_eth_volume
+    when 'Contributions' then eth_nxm_volume
+    when 'Withdrawals' then eth_eth_volume
+    when 'Balance Sheet' then null
+    when 'ETH Denominated Assets' then null
+    when 'ETH' then null
+    when 'stETH' then null
+    when 'rETH' then null
+    when 'cbBTC' then null
+    when 'Enzyme Vault' then null
+    when 'Aave aEthWETH' then null
+    when 'Stablecoin Denominated Assets' then null
+    when 'DAI' then null
+    when 'USDC' then null
+    when 'Cover Re' then null
+    when 'Aave debtUSDC' then null
+  end as eth_val,
+  null as usd_val
 from fin_combined fc
   cross join items i
 order by i.fi_id
