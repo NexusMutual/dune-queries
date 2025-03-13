839962348ae2bde6c914d9a5b45a862ffd8af827
tomfutago <tomfutago@gmail.com>
2025-03-13 09:37:45 +0000

    wip: add usd amounts

--- financials/_development/steth-sale.sql
+++ financials/_development/steth-sale.sql
@@ -3,6 +3,13 @@
 
 with
 
+prices as (
+  select
+    block_date,
+    avg_eth_usd_price
+  from query_4627588 -- Capital Pool - base root
+),
+
 steth_sales as (
   select
     block_time,
@@ -29,8 +36,27 @@ steth_sales_adjusted as (
     tx_hash
   from steth_sales
   where fill_type = 'funds returned'
+),
+
+steth_sales_agg as (
+  select
+    date_trunc('month', s.block_date) as block_month,
+    -1 * sum(coalesce(sa.sell_amount, s.sell_amount)) as eth_sell_amount,
+    -1 * sum(coalesce(sa.sell_amount, s.sell_amount) * p.avg_eth_usd_price) as usd_sell_amount,
+    sum(s.fill_amount) as eth_fill_amount,
+    sum(s.fill_amount * p.avg_eth_usd_price) as usd_fill_amount,
+    -1 * sum(coalesce(sa.sell_amount, s.sell_amount) - s.fill_amount) as eth_disposal_loss,
+    -1 * sum((coalesce(sa.sell_amount, s.sell_amount) - s.fill_amount) * p.avg_eth_usd_price) as usd_disposal_loss
+  from steth_sales s
+    inner join prices p on s.block_date = p.block_date
+    left join steth_sales_adjusted sa on sa.block_time = s.block_time and sa.tx_hash = s.tx_hash
+  where s.fill_type <> 'funds returned'
+  group by 1
 )
 
+select * from steth_sales_agg order by 1 desc
+
+/*
 select
   s.block_time,
   s.block_date,
@@ -43,3 +69,4 @@ select
 from steth_sales s
   left join steth_sales_adjusted sa on sa.block_time = s.block_time and sa.tx_hash = s.tx_hash
 where s.fill_type <> 'funds returned'
+*/
