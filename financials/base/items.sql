78d1af6bdfb5be9564533f435bdc15d12cdca892
tomfutago <tomfutago@gmail.com>
2025-03-13 16:35:40 +0000

    feat: updated and rearranged items

--- financials/base/items.sql
+++ financials/base/items.sql
@@ -1,21 +1,20 @@
 /*
 Revenue statement
-
-- Cash surplus
-    - Investment returns
-        - investment returns
-        - stablecoin impact
-    - Premiums - Claims
-    - Membership fees
-    - Reserve Movement (?)
-- Capital Movement
+- Cash Surplus
+    - Premiums
+    - Membership Fees
+    - Claims - Reimbursements
+- Investments Total
+    - breakdown of each investment line
+    - FX Impact
+- Capital Movement Total
     - Contributions (minting NXM for contributing ETH)
     - Withdrawals (withdrawing ETH for burning NXM)
+- Total Cash Movement
 
 Balance Sheet
-
-- ETH denominated Assets
-    - ETH, stETH, rETH etc
+- Crypto Denominated Assets
+    - ETH, stETH, rETH, cbBTC etc
 - Stablecoin Denominated Assets
     - DAI, USDC, Cover Re etc
 */
@@ -23,29 +22,35 @@ Balance Sheet
 with items (fi_id, scope, label, label_tab) as (
   values
   (1000, 'rs', 'Revenue Statement', '<b>Revenue Statement</b>'),
-  (1100, 'rs', 'Cash Surplus', '&nbsp; Cash Surplus'),
-  (1110, 'rs', 'Investments', '&nbsp; &nbsp; ⌄ Investments'),
-  (1113, 'rs', 'Investment Returns', '&nbsp; &nbsp; &nbsp; &nbsp; Investment Returns'),
-  (1116, 'rs', 'FX Impact', '&nbsp; &nbsp; &nbsp; &nbsp; FX Impact'),
-  (1120, 'rs', 'Premiums - Claims', '&nbsp; &nbsp; Premiums - Claims'),
-  (1130, 'rs', 'Membership Fees', '&nbsp; &nbsp; Membership Fees'),
-  --(1140, 'rs', 'Reserve Movement', '&nbsp; &nbsp; Reserve Movement'),
-  (1200, 'rs', 'Capital Movement', '&nbsp; Capital Movement'),
-  (1210, 'rs', 'Contributions', '&nbsp; &nbsp; Contributions'),
-  (1220, 'rs', 'Withdrawals', '&nbsp; &nbsp; Withdrawals'),
+  (1100, 'rs', 'Cash Surplus', '&nbsp; ⌄ Cash Surplus'),
+  (1110, 'rs', 'Premiums', '&nbsp; &nbsp; &nbsp; Premiums'),
+  (1120, 'rs', 'Membership Fees', '&nbsp; &nbsp; &nbsp; Membership Fees'),
+  (1130, 'rs', 'Claims - Reimbursements', '&nbsp; &nbsp; &nbsp; Claims - Reimbursements'),
+  (1200, 'rs', 'Investments Total', '&nbsp; ⌄ Investments Total'),
+  (1210, 'rs', 'stETH Return', '&nbsp; &nbsp; &nbsp; stETH Return'),
+  (1220, 'rs', 'rETH Return', '&nbsp; &nbsp; &nbsp; rETH Return'),
+  (1230, 'rs', 'Enzyme Vault Return', '&nbsp; &nbsp; &nbsp; Enzyme Vault Return'),
+  (1240, 'rs', 'Aave Net Return', '&nbsp; &nbsp; &nbsp; Aave Net Return'),
+  (1243, 'rs', 'aEthWETH Return', '&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; aEthWETH Return'),
+  (1246, 'rs', 'debtUSDC Return', '&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; debtUSDC Return'),
+  (1250, 'rs', 'FX Impact', '&nbsp; &nbsp; &nbsp; FX Impact'),
+  (1300, 'rs', 'Capital Movement', '&nbsp; ⌄ Capital Movement'),
+  (1310, 'rs', 'Contributions', '&nbsp; &nbsp; &nbsp; Contributions'),
+  (1320, 'rs', 'Withdrawals', '&nbsp; &nbsp; &nbsp; Withdrawals'),
+  (1400, 'rs', 'Total Cash Movement', '&nbsp; Total Cash Movement'),
   (2000, 'bs', 'Balance Sheet', '<b>Balance Sheet</b>'),
   (2100, 'bs', 'Crypto Denominated Assets', '&nbsp; ⌄ Crypto Denominated Assets'),
-  (2110, 'bs', 'ETH', '&nbsp; &nbsp; ETH'),
-  (2120, 'bs', 'stETH', '&nbsp; &nbsp; stETH'),
-  (2130, 'bs', 'rETH', '&nbsp; &nbsp; rETH'),
-  (2140, 'bs', 'cbBTC', '&nbsp; &nbsp; cbBTC'),
-  (2150, 'bs', 'Enzyme Vault', '&nbsp; &nbsp; Enzyme Vault'),
-  (2160, 'bs', 'Aave aEthWETH', '&nbsp; &nbsp; Aave aEthWETH'),
+  (2110, 'bs', 'ETH', '&nbsp; &nbsp; &nbsp; ETH'),
+  (2120, 'bs', 'stETH', '&nbsp; &nbsp; &nbsp; stETH'),
+  (2130, 'bs', 'rETH', '&nbsp; &nbsp; &nbsp; rETH'),
+  (2140, 'bs', 'cbBTC', '&nbsp; &nbsp; &nbsp; cbBTC'),
+  (2150, 'bs', 'Enzyme Vault', '&nbsp; &nbsp; &nbsp; Enzyme Vault'),
+  (2160, 'bs', 'Aave aEthWETH', '&nbsp; &nbsp; &nbsp; Aave aEthWETH'),
   (2200, 'bs', 'Stablecoin Denominated Assets', '&nbsp; ⌄ Stablecoin Denominated Assets'),
-  (2210, 'bs', 'DAI', '&nbsp; &nbsp; DAI'),
-  (2220, 'bs', 'USDC', '&nbsp; &nbsp; USDC'),
-  (2230, 'bs', 'Cover Re', '&nbsp; &nbsp; Cover Re'),
-  (2240, 'bs', 'Aave debtUSDC', '&nbsp; &nbsp; Aave debtUSDC')
+  (2210, 'bs', 'DAI', '&nbsp; &nbsp; &nbsp; DAI'),
+  (2220, 'bs', 'USDC', '&nbsp; &nbsp; &nbsp; USDC'),
+  (2230, 'bs', 'Cover Re', '&nbsp; &nbsp; &nbsp; Cover Re'),
+  (2240, 'bs', 'Aave debtUSDC', '&nbsp; &nbsp; &nbsp; Aave debtUSDC')
 )
 
 select fi_id, scope, label, label_tab
