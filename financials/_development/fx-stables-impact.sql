d51d25ec98ebd0562ab86ffa1fd61e26e7a2e43a
tomfutago <tomfutago@gmail.com>
2025-03-13 15:04:23 +0000

    wip: add usd amounts calc

--- financials/_development/fx-stables-impact.sql
+++ financials/_development/fx-stables-impact.sql
@@ -11,22 +11,33 @@ prices as (
 capital_pool as (
   select
     date_trunc('month', block_date) as block_month,
-    debt_usdc,
-    lag(debt_usdc, 1) over (order by block_date) as debt_usdc_prev,
-    dai,
-    lag(dai, 1) over (order by block_date) as dai_prev,
-    usdc,
-    lag(usdc, 1) over (order by block_date) as usdc_prev,
-    cover_re,
-    lag(cover_re, 1) over (order by block_date) as cover_re_prev
+    eth_debt_usdc,
+    lag(eth_debt_usdc, 1) over (order by block_date) as eth_debt_usdc_prev,
+    eth_dai,
+    lag(eth_dai, 1) over (order by block_date) as eth_dai_prev,
+    eth_usdc,
+    lag(eth_usdc, 1) over (order by block_date) as eth_usdc_prev,
+    eth_cover_re,
+    lag(eth_cover_re, 1) over (order by block_date) as eth_cover_re_prev,
+    usd_debt_usdc,
+    lag(usd_debt_usdc, 1) over (order by block_date) as usd_debt_usdc_prev,
+    usd_dai,
+    lag(usd_dai, 1) over (order by block_date) as usd_dai_prev,
+    usd_usdc,
+    lag(usd_usdc, 1) over (order by block_date) as usd_usdc_prev,
+    usd_cover_re,
+    lag(usd_cover_re, 1) over (order by block_date) as usd_cover_re_prev
   from (
     select
       block_date,
-      avg_capital_pool_eth_total as capital_pool,
-      avg_aave_debt_usdc_eth_total as debt_usdc,
-      avg_dai_eth_total as dai,
-      avg_usdc_eth_total as usdc,
-      avg_cover_re_usdc_eth_total as cover_re,
+      avg_aave_debt_usdc_eth_total as eth_debt_usdc,
+      avg_dai_eth_total as eth_dai,
+      avg_usdc_eth_total as eth_usdc,
+      avg_cover_re_usdc_eth_total as eth_cover_re,
+      avg_aave_debt_usdc_usd_total as usd_debt_usdc,
+      avg_dai_usd_total as usd_dai,
+      avg_usdc_usd_total as usd_usdc,
+      avg_cover_re_usdc_usd_total as usd_cover_re,
       row_number() over (partition by date_trunc('month', block_date) order by block_date desc) as rn
     from query_4627588 -- Capital Pool - base root
   ) t
@@ -70,10 +81,15 @@ stables_fx_impact as (
     p.eth_usd_price_start,
     p.eth_usd_price_end,
     -- stables expressed in ETH
-    s.dai_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.dai_prev as dai_fx_change,
-    s.usdc_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.usdc_prev as usdc_fx_change,
-    s.cover_re_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.cover_re_prev as cover_re_fx_change,
-    s.debt_usdc_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.debt_usdc_prev as debt_usdc_fx_change
+    s.eth_dai_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_dai_prev as eth_dai_fx_change,
+    s.eth_usdc_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_usdc_prev as eth_usdc_fx_change,
+    s.eth_cover_re_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_cover_re_prev as eth_cover_re_fx_change,
+    s.eth_debt_usdc_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_debt_usdc_prev as eth_debt_usdc_fx_change,
+    -- stables expressed in USD
+    (s.eth_dai_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_dai_prev) * p.eth_usd_price_end as usd_dai_fx_change,
+    (s.eth_usdc_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_usdc_prev) * p.eth_usd_price_end as usd_usdc_fx_change,
+    (s.eth_cover_re_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_cover_re_prev) * p.eth_usd_price_end as usd_cover_re_fx_change,
+    (s.eth_debt_usdc_prev * p.eth_usd_price_start / p.eth_usd_price_end - s.eth_debt_usdc_prev) * p.eth_usd_price_end as usd_debt_usdc_fx_change
   from capital_pool s
     inner join prices_start_end p on s.block_month = p.block_month
 )
@@ -82,10 +98,15 @@ select
   block_month,
   eth_usd_price_start,
   eth_usd_price_end,
-  dai_fx_change,
-  usdc_fx_change,
-  cover_re_fx_change,
-  debt_usdc_fx_change,
-  dai_fx_change + usdc_fx_change + cover_re_fx_change + debt_usdc_fx_change as fx_change
+  /*eth_dai_fx_change,
+  eth_usdc_fx_change,
+  eth_cover_re_fx_change,
+  eth_debt_usdc_fx_change,*/
+  eth_dai_fx_change + eth_usdc_fx_change + eth_cover_re_fx_change + eth_debt_usdc_fx_change as eth_fx_change,
+  /*usd_dai_fx_change,
+  usd_usdc_fx_change,
+  usd_cover_re_fx_change,
+  usd_debt_usdc_fx_change,*/
+  usd_dai_fx_change + usd_usdc_fx_change + usd_cover_re_fx_change + usd_debt_usdc_fx_change as usd_fx_change
 from stables_fx_impact
 order by 1 desc
