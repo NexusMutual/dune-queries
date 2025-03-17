b187712f72c6aaed3ac867b2d01ab0edcbe2d59d
tomfutago <tomfutago@gmail.com>
2025-03-17 16:36:00 +0000

    refactor: use closing fx rate to calc usd amounts, add kiln rewards

--- financials/base/balance-sheet.sql
+++ financials/base/balance-sheet.sql
@@ -1,5 +1,12 @@
 with
 
+prices as (
+  select
+    block_date,
+    avg_eth_usd_price
+  from query_4627588 -- Capital Pool - base root
+),
+
 capital_pool as (
   select
     date_trunc('month', block_date) as block_month,
@@ -14,19 +21,7 @@ capital_pool as (
     eth_dai,
     eth_usdc,
     eth_cbbtc,
-    eth_cover_re,
-    usd_capital_pool,
-    lag(usd_capital_pool, 1) over (order by block_date) as usd_capital_pool_prev,
-    usd_eth,
-    usd_steth,
-    usd_reth,
-    usd_nxmty,
-    usd_aweth,
-    usd_debt_usdc,
-    usd_dai,
-    usd_usdc,
-    usd_cbbtc,
-    usd_cover_re
+    eth_cover_re
   from (
     select
       block_date,
@@ -41,51 +36,74 @@ capital_pool as (
       avg_usdc_eth_total as eth_usdc,
       avg_cbbtc_eth_total as eth_cbbtc,
       avg_cover_re_usdc_eth_total as eth_cover_re,
-      avg_capital_pool_usd_total as usd_capital_pool,
-      avg_eth_usd_total as usd_eth,
-      avg_steth_usd_total as usd_steth,
-      avg_reth_usd_total as usd_reth,
-      avg_nxmty_usd_total as usd_nxmty,
-      avg_aave_collateral_weth_usd_total as usd_aweth,
-      avg_aave_debt_usdc_usd_total as usd_debt_usdc,
-      avg_dai_usd_total as usd_dai,
-      avg_usdc_usd_total as usd_usdc,
-      avg_cbbtc_usd_total as usd_cbbtc,
-      avg_cover_re_usdc_usd_total as usd_cover_re,
       row_number() over (partition by date_trunc('month', block_date) order by block_date desc) as rn
     from query_4627588 -- Capital Pool - base root
   ) t
   where t.rn = 1
   order by 1 desc
   limit 12 -- 12 months rolling
+),
+
+kiln_rewards as (
+  select
+    date_trunc('month', t.seq_date) as block_month,
+    t.kiln_rewards as eth_kiln_rewards_total,
+    lag(t.kiln_rewards, 1, 0) over (order by t.seq_date) as eth_kiln_rewards_total_prev,
+    t.kiln_rewards - lag(t.kiln_rewards, 1, 0) over (order by t.seq_date) as eth_kiln_rewards,
+    lag(t.kiln_rewards, 1, 0) over (order by t.seq_date) - lag(t.kiln_rewards, 2, 0) over (order by t.seq_date) as eth_kiln_rewards_prev
+  from (
+      select
+        seq_date,
+        kiln_rewards,
+        row_number() over (partition by date_trunc('month', seq_date) order by seq_date desc) as rn
+      from query_4830965 -- kiln rewards
+    ) t
+  where t.rn = 1
+),
+
+prices_start_end as (
+  select
+    date_trunc('month', block_date) as block_month,
+    lag(avg_eth_usd_price, 1) over (order by block_date) as eth_usd_price_start, -- start: last of the previous month
+    avg_eth_usd_price as eth_usd_price_end -- end: last of the month
+  from (
+    select
+      block_date,
+      avg_eth_usd_price,
+      row_number() over (partition by date_trunc('month', block_date) order by block_date desc) as rn
+    from prices
+  ) t
+  where rn = 1
 )
 
 select
   --date_format(block_month, '%Y-%m') as block_month,
-  block_month,
-  eth_capital_pool,
-  eth_capital_pool_prev,
-  eth_eth,
-  eth_steth,
-  eth_reth,
-  eth_nxmty,
-  eth_aweth,
-  eth_debt_usdc,
-  eth_dai,
-  eth_usdc,
-  eth_cbbtc,
-  eth_cover_re,
-  usd_capital_pool,
-  usd_capital_pool_prev,
-  usd_eth,
-  usd_steth,
-  usd_reth,
-  usd_nxmty,
-  usd_aweth,
-  usd_debt_usdc,
-  usd_dai,
-  usd_usdc,
-  usd_cbbtc,
-  usd_cover_re
-from capital_pool
+  cp.block_month,
+  cp.eth_capital_pool + coalesce(kr.eth_kiln_rewards, 0) as eth_capital_pool,
+  cp.eth_capital_pool_prev + coalesce(kr.eth_kiln_rewards_prev, 0) as eth_capital_pool_prev,
+  cp.eth_eth,
+  cp.eth_steth,
+  cp.eth_reth,
+  cp.eth_nxmty + coalesce(kr.eth_kiln_rewards, 0) as eth_nxmty,
+  cp.eth_aweth,
+  cp.eth_debt_usdc,
+  cp.eth_dai,
+  cp.eth_usdc,
+  cp.eth_cbbtc,
+  cp.eth_cover_re,
+  (cp.eth_capital_pool + coalesce(kr.eth_kiln_rewards, 0)) * p.eth_usd_price_end as usd_capital_pool,
+  (cp.eth_capital_pool_prev + coalesce(kr.eth_kiln_rewards_prev, 0)) * p.eth_usd_price_end as usd_capital_pool_prev,
+  cp.eth_eth * p.eth_usd_price_end as usd_eth,
+  cp.eth_steth * p.eth_usd_price_end as usd_steth,
+  cp.eth_reth * p.eth_usd_price_end as usd_reth,
+  (cp.eth_nxmty + coalesce(kr.eth_kiln_rewards, 0)) * p.eth_usd_price_end as usd_nxmty,
+  cp.eth_aweth * p.eth_usd_price_end as usd_aweth,
+  cp.eth_debt_usdc * p.eth_usd_price_end as usd_debt_usdc,
+  cp.eth_dai * p.eth_usd_price_end as usd_dai,
+  cp.eth_usdc * p.eth_usd_price_end as usd_usdc,
+  cp.eth_cbbtc * p.eth_usd_price_end as usd_cbbtc,
+  cp.eth_cover_re * p.eth_usd_price_end as usd_cover_re
+from capital_pool cp
+  inner join prices_start_end p on cp.block_month = p.block_month
+  left join kiln_rewards kr on cp.block_month = kr.block_month
 order by 1 desc
