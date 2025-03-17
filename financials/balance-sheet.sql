ac8a2d8afedcc6a76158d942b32337a2ce052869
tomfutago <tomfutago@gmail.com>
2025-03-17 09:30:00 +0000

    chore: add "pretty" zero

--- financials/balance-sheet.sql
+++ financials/balance-sheet.sql
@@ -40,34 +40,34 @@ select
   case i.label
     when 'Balance Sheet' then null
     when 'Crypto Denominated Assets' then eth_eth + eth_steth + eth_reth + eth_cbbtc + eth_nxmty + eth_aweth
-    when 'ETH' then eth_eth
-    when 'stETH' then eth_steth
-    when 'rETH' then eth_reth
-    when 'cbBTC' then eth_cbbtc
-    when 'Enzyme Vault' then eth_nxmty
-    when 'Aave aEthWETH' then eth_aweth
+    when 'ETH' then coalesce(nullif(eth_eth, 0), 1e-6)
+    when 'stETH' then coalesce(nullif(eth_steth, 0), 1e-6)
+    when 'rETH' then coalesce(nullif(eth_reth, 0), 1e-6)
+    when 'cbBTC' then coalesce(nullif(eth_cbbtc, 0), 1e-6)
+    when 'Enzyme Vault' then coalesce(nullif(eth_nxmty, 0), 1e-6)
+    when 'Aave aEthWETH' then coalesce(nullif(eth_aweth, 0), 1e-6)
     when 'Stablecoin Denominated Assets' then eth_dai + eth_usdc + eth_cover_re + eth_debt_usdc
-    when 'DAI' then eth_dai
-    when 'USDC' then eth_usdc
-    when 'Cover Re' then eth_cover_re
-    when 'Aave debtUSDC' then eth_debt_usdc
-    when 'Total Balance' then eth_capital_pool
+    when 'DAI' then coalesce(nullif(eth_dai, 0), 1e-6)
+    when 'USDC' then coalesce(nullif(eth_usdc, 0), 1e-6)
+    when 'Cover Re' then coalesce(nullif(eth_cover_re, 0), 1e-6)
+    when 'Aave debtUSDC' then coalesce(nullif(eth_debt_usdc, 0), 1e-6)
+    when 'Total Balance' then coalesce(nullif(eth_capital_pool, 0), 1e-6)
   end as eth_val,
   case i.label
     when 'Balance Sheet' then null
     when 'Crypto Denominated Assets' then usd_eth + usd_steth + usd_reth + usd_cbbtc + usd_nxmty + usd_aweth
-    when 'ETH' then usd_eth
-    when 'stETH' then usd_steth
-    when 'rETH' then usd_reth
-    when 'cbBTC' then usd_cbbtc
-    when 'Enzyme Vault' then usd_nxmty
-    when 'Aave aEthWETH' then usd_aweth
+    when 'ETH' then coalesce(nullif(usd_eth, 0), 1e-6)
+    when 'stETH' then coalesce(nullif(usd_steth, 0), 1e-6)
+    when 'rETH' then coalesce(nullif(usd_reth, 0), 1e-6)
+    when 'cbBTC' then coalesce(nullif(usd_cbbtc, 0), 1e-6)
+    when 'Enzyme Vault' then coalesce(nullif(usd_nxmty, 0), 1e-6)
+    when 'Aave aEthWETH' then coalesce(nullif(usd_aweth, 0), 1e-6)
     when 'Stablecoin Denominated Assets' then usd_dai + usd_usdc + usd_cover_re + usd_debt_usdc
-    when 'DAI' then usd_dai
-    when 'USDC' then usd_usdc
-    when 'Cover Re' then usd_cover_re
-    when 'Aave debtUSDC' then usd_debt_usdc
-    when 'Total Balance' then usd_capital_pool
+    when 'DAI' then coalesce(nullif(usd_dai, 0), 1e-6)
+    when 'USDC' then coalesce(nullif(usd_usdc, 0), 1e-6)
+    when 'Cover Re' then coalesce(nullif(usd_cover_re, 0), 1e-6)
+    when 'Aave debtUSDC' then coalesce(nullif(usd_debt_usdc, 0), 1e-6)
+    when 'Total Balance' then coalesce(nullif(usd_capital_pool, 0), 1e-6)
   end as usd_val
 from balance_sheet bs
   cross join items i
