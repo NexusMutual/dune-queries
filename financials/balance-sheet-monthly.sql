de7955687ab102ddb1e6b6fe1cf07f5466ed2e7f
tomfutago <tomfutago@gmail.com>
2025-03-18 13:32:10 +0000

    feat: add totals

--- financials/balance-sheet-monthly.sql
+++ financials/balance-sheet-monthly.sql
@@ -2,27 +2,31 @@ select
   --date_format(block_month, '%Y-%m') as block_month,
   block_month,
   eth_capital_pool,
+  eth_eth + eth_steth + eth_reth + eth_cbbtc + eth_nxmty + eth_aweth as eth_crypto_assets,
   eth_eth,
   eth_steth,
   eth_reth,
   eth_nxmty,
   eth_aweth,
+  eth_dai + eth_usdc + eth_cover_re + eth_debt_usdc as eth_stablecoin_assets,
   eth_debt_usdc,
   eth_dai,
   eth_usdc,
   eth_cbbtc,
   eth_cover_re,
   usd_capital_pool,
+  usd_eth + usd_steth + usd_reth + usd_cbbtc + usd_nxmty + usd_aweth as usd_crypto_assets,
   usd_eth,
   usd_steth,
   usd_reth,
   usd_nxmty,
   usd_aweth,
+  usd_dai + usd_usdc + usd_cover_re + usd_debt_usdc as usd_stablecoin_assets,
   usd_debt_usdc,
   usd_dai,
   usd_usdc,
   usd_cbbtc,
   usd_cover_re
-from capital_pool
+from query_4841979 -- balance sheet
 where block_month >= date_add('month', -12, current_date)
 order by 1 desc
