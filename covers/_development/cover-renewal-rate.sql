fd2cd7b15952ef041b55fe14c13d883ed3ed9ae3
tomfutago <tomfutago@gmail.com>
2025-03-31 10:05:37 +0100

    wip: exclude opencover

--- covers/_development/cover-renewal-rate.sql
+++ covers/_development/cover-renewal-rate.sql
@@ -6,6 +6,13 @@ with renewals as (
     max(cover_end_date) as last_cover_date,
     date_diff('month', min(cover_start_date), max(cover_end_date)) as coverage_months
   from query_4599092
+  where commission_destination not in (
+      -- OpenCover:
+      0xe4994082a0e7f38b565e6c5f4afd608de5eddfbb,
+      0x40329f3e27dd3fe228799b4a665f6f104c2ab6b4,
+      0x5f2b6e70aa6a217e9ecd1ed7d0f8f38ce9a348a2,
+      0x02bdacb2c3baa8a12d3957f3bd8637d6d2b35f10
+    )
   group by 1
 ),
 
@@ -20,6 +27,6 @@ customer_counts as (
 select
   total_cover_buyers,
   renewal_cover_buyers,
-  round(100.0 * renewal_cover_buyers / total_cover_buyers, 2) as renewal_rate_percentage,
+  renewal_cover_buyers * 1.0000 / total_cover_buyers as renewal_rate_percentage,
   avg_renewal_months
 from customer_counts
