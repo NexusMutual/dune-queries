a33de29fb722c96b4e481f2841b4b06617b4740d
tomfutago <tomfutago@gmail.com>
2025-04-17 13:15:56 +0100

    feat: final kiln rewards

--- financials/base/kiln-rewards.sql
+++ financials/base/kiln-rewards.sql
@@ -3,24 +3,24 @@ with
 constants as (
   select 
     181.45 as kiln_first_round,
-    190.881 as kiln_current,
+    194.7714625 as kiln_accrued_rewards,
     date '2023-08-30' as kiln_deposit_date,
     date '2024-04-24' as kiln_claim_start_date,
     date '2024-04-26' as kiln_claim_end_date,
-    date '2025-04-01' as latest_date -- ICN generated
+    date '2025-04-15' as kiln_final_withdrawal_date
 ),
 
 date_sequence as (
   select d.seq_date 
   from constants,
-    unnest(sequence(kiln_deposit_date, latest_date, interval '1' day)) as d(seq_date)
+    unnest(sequence(kiln_deposit_date, kiln_final_withdrawal_date, interval '1' day)) as d(seq_date)
 ),
 
 calc as (
   select 
     *,
     date_diff('day', kiln_claim_start_date, kiln_deposit_date) as days_first_kiln_rewards,
-    date_diff('day', latest_date, kiln_claim_end_date) as days_current_kiln_rewards
+    date_diff('day', kiln_final_withdrawal_date, kiln_claim_end_date) as days_current_kiln_rewards
   from constants
 ),
 
@@ -31,7 +31,7 @@ rewards as (
       when d.seq_date <= c.kiln_claim_start_date
         then c.kiln_first_round / c.days_first_kiln_rewards * date_diff('day', d.seq_date, c.kiln_deposit_date)
       when d.seq_date >= c.kiln_claim_end_date
-        then c.kiln_current / c.days_current_kiln_rewards * date_diff('day', d.seq_date, c.kiln_claim_end_date)
+        then c.kiln_accrued_rewards / c.days_current_kiln_rewards * date_diff('day', d.seq_date, c.kiln_claim_end_date)
       else 0 
     end as kiln_rewards
   from date_sequence d, calc c
