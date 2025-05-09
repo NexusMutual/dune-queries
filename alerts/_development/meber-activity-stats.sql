c4c5c5f5ca355f58d3c3d2fe82845f839d695e05
tomfutago <tomfutago@gmail.com>
2025-05-09 13:56:35 +0100

    chore: use bae query & move folder

--- alerts/_development/meber-activity-stats.sql
+++ alerts/_development/meber-activity-stats.sql
@@ -1,59 +1,7 @@
 with
 
-whitelist_raw as (
-  -- whitelist addition
-  select
-    call_block_time as block_time,
-    _member as member,
-    1 as counter,
-    true as is_active
-  from nexusmutual_ethereum.tokencontroller_call_addtowhitelist
-  where call_success
-  union all
-  -- whitelist removal
-  select
-    call_block_time as block_time,
-    _member as member,
-    -1 as counter,
-    false as is_active
-  from nexusmutual_ethereum.tokencontroller_call_removefromwhitelist
-  where call_success
-  union all
-  -- membership switch - previous member
-  select
-    evt_block_time as block_time,
-    previousMember as member,
-    -1 as counter,
-    false as is_active
-  from nexusmutual_ethereum.memberroles_evt_switchedmembership
-  union all
-  -- membership switch - new member
-  select
-    evt_block_time as block_time,
-    newMember as member,
-    1 as counter,
-    true as is_active
-  from nexusmutual_ethereum.memberroles_evt_switchedmembership
-),
-
-whitelist as (
-  select distinct
-    block_time,
-    member,
-    counter,
-    is_active
-  from whitelist_raw
-),
-
-members as (
-  select
-    block_time,
-    member,
-    counter,
-    is_active,
-    sum(counter) over (order by block_time, member) as active_members,
-    count(distinct member) over (order by block_time, member) as all_time_members
-  from whitelist
+member_activity as (
+  select * from query_5097910 -- member activity base
 ),
 
 latest as (
@@ -61,7 +9,7 @@ latest as (
     max(block_time) as latest_block_time,
     max_by(active_members, block_time) as active_members_now,
     max_by(all_time_members, block_time) as all_time_members_now
-  from members
+  from member_activity
 ),
 
 historical as (
@@ -69,7 +17,7 @@ historical as (
     max(case when date_diff('day', block_time, latest_block_time) between 29 and 31 then active_members end) as active_members_30d_ago,
     max(case when date_diff('day', block_time, latest_block_time) between 89 and 91 then active_members end) as active_members_90d_ago,
     max(case when date_diff('day', block_time, latest_block_time) between 179 and 181 then active_members end) as active_members_180d_ago
-  from members, latest
+  from member_activity, latest
 ),
 
 member_activity_stats as (
@@ -91,7 +39,5 @@ member_activity_stats as (
     round(100.0 * (l.active_members_now - h.active_members_180d_ago) / nullif(h.active_members_180d_ago, 0), 2) as pct_change_180d
   from latest l, historical h
 )
-
---select distinct member from members -- list of all the members
 
 select * from member_activity_stats
