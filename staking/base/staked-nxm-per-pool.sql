ee1550ac0034885f27a5ee312e431a37a77bee0f
tomfutago <tomfutago@gmail.com>
2025-05-16 15:46:35 +0100

    refactor: pool_id & utils.days

--- staking/base/staked-nxm-per-pool.sql
+++ staking/base/staked-nxm-per-pool.sql
@@ -5,32 +5,25 @@ staking_pools as (
     sp.pool_id,
     sp.pool_address,
     se.first_stake_event_date
-  --from query_3859935 sp -- staking pools base (fallback) query
-  --from nexusmutual_ethereum.staking_pools sp
-  from query_4167546 sp -- staking pools - spell de-duped
+  --from nexusmutual_ethereum.staking_pools_list sp
+  from query_5128062 sp -- staking pools list - base query
     inner join (
       select
-        pool_address,
+        pool_id,
         cast(min(block_time) as date) as first_stake_event_date
-      --from query_3609519 -- staking events
-      from nexusmutual_ethereum.staking_events
+      from query_3609519 -- staking events
+      --from nexusmutual_ethereum.staking_events
       group by 1
-    ) se on sp.pool_address = se.pool_address
+    ) se on sp.pool_id = se.pool_id
 ),
 
 staking_pool_day_sequence as (
   select
     sp.pool_id,
     sp.pool_address,
-    s.block_date
-  from staking_pools sp
-    cross join unnest (
-      sequence(
-        cast(date_trunc('day', sp.first_stake_event_date) as timestamp),
-        cast(date_trunc('day', now()) as timestamp),
-        interval '1' day
-      )
-    ) as s(block_date)
+    d.timestamp as block_date
+  from utils.days d
+    inner join staking_pools sp on d.timestamp >=sp.first_stake_event_date
 ),
 
 staked_nxm_per_pool as (
@@ -48,9 +41,9 @@ staked_nxm_per_pool as (
         d.pool_address,
         sum(se.amount) as total_amount
       from staking_pool_day_sequence d
-        --left join query_3619534 se -- staking deposit extensions base query
-        left join nexusmutual_ethereum.staking_deposit_extensions se
-          on d.pool_address = se.pool_address
+        left join query_3619534 se -- staking deposit extensions base query
+        --left join nexusmutual_ethereum.staking_deposit_extensions se
+          on d.pool_id = se.pool_id
          and d.block_date >= se.stake_start_date
          and d.block_date < se.stake_end_date
       group by 1, 2, 3
@@ -62,9 +55,9 @@ staked_nxm_per_pool as (
         d.pool_address,
         sum(se.amount) as total_amount
       from staking_pool_day_sequence d
-        --left join query_3609519 se -- staking events
-        left join nexusmutual_ethereum.staking_events se
-          on d.pool_address = se.pool_address
+        left join query_3609519 se -- staking events
+        --left join nexusmutual_ethereum.staking_events se
+          on d.pool_id = se.pool_id
          and d.block_date >= se.block_date
          and d.block_date < coalesce(se.tranche_expiry_date, current_date)
       where flow_type in ('withdraw', 'stake burn')
