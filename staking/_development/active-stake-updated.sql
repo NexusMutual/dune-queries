7fe7886e6af4b1df106a66795994d068256b79a9
tomfutago <tomfutago@gmail.com>
2025-05-16 16:08:05 +0100

    refactor: active stake updated daily

--- staking/_development/active-stake-updated.sql
+++ staking/_development/active-stake-updated.sql
@@ -2,43 +2,44 @@ with
 
 staking_pools as (
   select
-    evt_block_time as block_time,
-    poolId as pool_id,
-    stakingPoolAddress as pool_address,
-    evt_tx_hash as tx_hash
-  from nexusmutual_ethereum.StakingPoolFactory_evt_StakingPoolCreated
-),
-
-staking_pool_names as (
-  select pool_id, pool_name
-  from query_3833996 -- staking pool names base (fallback) query
+    pool_id,
+    pool_address
+  --from nexusmutual_ethereum.staking_pools_list
+  from query_5128062 -- staking pools list - base query
 ),
 
 active_stake_updated as (
   select
-    contract_address as pool_address,
-    evt_block_time as block_time,
-    evt_block_date as block_date,
-    evt_block_number as block_number,
-    activeStake as active_stake,
-    stakeSharesSupply as stake_shares_supply,
-    evt_tx_hash as tx_hash,
-    row_number() over (partition by contract_address order by evt_block_time desc) as rn
-  from nexusmutual_ethereum.stakingpool_evt_activestakeupdated
+    sp.pool_id,
+    sp.pool_address,
+    asu.evt_block_time as block_time,
+    asu.evt_block_date as block_date,
+    asu.evt_block_number as block_number,
+    asu.activeStake / 1e18 as active_stake,
+    asu.stakeSharesSupply as stake_shares_supply,
+    asu.evt_tx_hash as tx_hash
+  from nexusmutual_ethereum.StakingPool_evt_ActiveStakeUpdated asu
+    inner join staking_pools sp on asu.contract_address = sp.pool_address
+),
+
+active_stake_updated_daily as (
+  select
+    pool_id,
+    pool_address,
+    block_date,
+    max_by(active_stake, block_date) as active_stake,
+    max_by(stake_shares_supply, block_date) as stake_shares_supply,
+    max_by(tx_hash, block_date) as tx_hash
+  from active_stake_updated
+  group by 1, 2, 3
 )
 
 select
-  sp.pool_id,
-  sp.pool_address,
-  spn.pool_name,
-  asu.block_time,
-  asu.block_date,
-  asu.block_number,
-  asu.active_stake / 1e18 as active_stake,
-  asu.stake_shares_supply,
-  asu.tx_hash
-from staking_pools sp
-  inner join active_stake_updated asu on sp.pool_address = asu.pool_address
-  left join staking_pool_names spn on sp.pool_id = spn.pool_id
-where asu.rn = 1
-order by 1
+  pool_id,
+  pool_address,
+  block_date,
+  active_stake,
+  stake_shares_supply,
+  tx_hash
+from active_stake_updated_daily
+order by 1, 2, 3
