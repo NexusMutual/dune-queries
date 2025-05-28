4220a3d07284568661fd6fb86ac7255fb461325d
tomfutago <tomfutago@gmail.com>
2025-05-28 12:31:37 +0100

    refactor: more flow exceptions

--- staking/base/staking-deposits-ordered.sql
+++ staking/base/staking-deposits-ordered.sql
@@ -35,39 +35,66 @@ deposits as (
   --from query_3609519 -- staking events - base
   from nexusmutual_ethereum.staking_events
   where flow_type in ('deposit', 'deposit extended')
+),
+
+deposits_enriched as (
+  select
+    block_time,
+    case
+      -- if there is a deposit following another deposit on the same tranche:
+      when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit' and prev_init_tranche_id = tranche_id then 'deposit addon'
+      -- if there is a deposit following a deposit extended on the same tranche - scenario 1:
+      -- (ex token_id = 31, tx = 0x1aabd858abbb4bffb7723c81c9e81206eba924b74ce377c310728607dca5c7aa)
+      when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and tranche_id = prev_new_tranche_id then 'deposit ext addon'
+      -- if there is a deposit following a deposit extended on the same tranche - scenario 2:
+      -- (ex token_id = 39, tx = 0x0131fe7eddf72cfca03c7926dab002061c78f145ed240139d8c2612726735a8d)
+      when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and tranche_id = next_init_tranche_id then 'deposit ext addon'
+      else flow_type
+    end as flow_type,
+    block_date as stake_start_date, -- stays static
+    -- adjust stake_end_date to either the next deposit or the tranche expiry date
+    case
+      when flow_type = 'deposit extended' and next_flow_type = 'deposit' and new_tranche_id = next_new_tranche_id then coalesce(next_init_block_date, tranche_expiry_date)
+      when flow_type = 'deposit' and next_flow_type <> 'deposit extended' and next_init_tranche_id <> tranche_id then tranche_expiry_date
+      when flow_type = 'deposit extended' and next_flow_type = 'deposit' then tranche_expiry_date
+      when next_init_block_date > tranche_expiry_date then tranche_expiry_date
+      else coalesce(next_init_block_date, tranche_expiry_date)
+    end as stake_end_date,
+    pool_id,
+    pool_address,
+    token_id,
+    tranche_id,
+    init_tranche_id,
+    new_tranche_id,
+    tranche_expiry_date,
+    is_active,
+    amount,
+    topup_amount,
+    user,
+    evt_index,
+    tx_hash,
+    deposit_rn
+  from deposits
 )
 
 select
   block_time,
-  case
-    -- if there is a deposit following another deposit on the same tranche:
-    when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit' and prev_init_tranche_id = tranche_id then 'deposit addon'
-    -- if there is a deposit following a deposit extended on the same tranche:
-    when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and prev_new_tranche_id = tranche_id then 'deposit ext addon'
-    else flow_type
-  end as flow_type,
-  block_date as stake_start_date, -- stays static
-  -- adjust stake_end_date to either the next deposit or the tranche expiry date
-  case
-    when flow_type = 'deposit extended' and next_flow_type = 'deposit' and new_tranche_id = next_new_tranche_id then coalesce(next_init_block_date, tranche_expiry_date)
-    when flow_type = 'deposit' and next_flow_type <> 'deposit extended' and next_init_tranche_id <> tranche_id then tranche_expiry_date
-    when flow_type = 'deposit extended' and next_flow_type = 'deposit' then tranche_expiry_date
-    when next_init_block_date > tranche_expiry_date then tranche_expiry_date
-    else coalesce(next_init_block_date, tranche_expiry_date)
-  end as stake_end_date,
+  flow_type,
+  stake_start_date,
+  stake_end_date,
   pool_id,
   pool_address,
   token_id,
   -- for 'deposit ext addon' re-shuffle deposit-like fields to emulate deposit extension
-  case when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and prev_new_tranche_id = tranche_id then null else tranche_id end as tranche_id,
-  case when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and prev_new_tranche_id = tranche_id then tranche_id else init_tranche_id end as init_tranche_id,
-  case when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and prev_new_tranche_id = tranche_id then tranche_id else new_tranche_id end as new_tranche_id,
+  if(flow_type = 'deposit ext addon', null, tranche_id) as tranche_id,
+  if(flow_type = 'deposit ext addon', tranche_id, init_tranche_id) as init_tranche_id,
+  if(flow_type = 'deposit ext addon', tranche_id, new_tranche_id) as new_tranche_id,
   tranche_expiry_date,
   is_active,
-  case when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and prev_new_tranche_id = tranche_id then null else amount end as amount,
-  case when token_id = prev_token_id and flow_type = 'deposit' and prev_flow_type = 'deposit extended' and prev_new_tranche_id = tranche_id then amount else topup_amount end as topup_amount,
+  if(flow_type = 'deposit ext addon', null, amount) as amount,
+  if(flow_type = 'deposit ext addon', amount, topup_amount) as topup_amount,
   user,
   evt_index,
   tx_hash,
   deposit_rn
-from deposits
+from deposits_enriched
