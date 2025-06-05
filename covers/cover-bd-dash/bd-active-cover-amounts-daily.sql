bad6ec41781ffe0e599dc4ff88da29b97ab2ff57
tomfutago <tomfutago@gmail.com>
2025-06-05 09:49:34 +0100

    feat: eth/usd price

--- covers/cover-bd-dash/bd-active-cover-amounts-daily.sql
+++ covers/cover-bd-dash/bd-active-cover-amounts-daily.sql
@@ -1,5 +1,14 @@
+with daily_avg_prices as (
+  select
+    block_date,
+    avg_eth_usd_price
+  --from query_3789851 -- prices base (fallback) query
+  from nexusmutual_ethereum.capital_pool_prices
+)
+
 select
-  block_date,
+  c.block_date,
+  p.avg_eth_usd_price,
   active_cover,
   eth_active_cover,
   eth_eth_active_cover,
@@ -32,6 +41,7 @@ select
   usd_active_premium / active_cover as mean_usd_active_premium,
   median_usd_active_premium
 --from query_3889661 -- BD active cover base
-from nexusmutual_ethereum.covers_daily_agg
-where block_date >= now() - interval '6' month
+from nexusmutual_ethereum.covers_daily_agg c
+  inner join daily_avg_prices p on c.block_date = p.block_date
+where c.block_date >= now() - interval '6' month
 order by 1 desc
