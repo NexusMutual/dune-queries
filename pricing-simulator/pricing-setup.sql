598fb52e3b30adc3e6b32ed063afc0cf10c3a904
tomfutago <tomfutago@gmail.com>
2025-06-20 14:00:29 +0100

    refactor: apply permutations

--- pricing-simulator/pricing-setup.sql
+++ pricing-simulator/pricing-setup.sql
@@ -1,9 +1,11 @@
-with pricing_setup (id, time_period, time_price_ratio, deductible, deductible_price_ratio) as (
+with pricing_setup (id, time_period, deductible, time_price_ratio, deductible_price_ratio) as (
   values
-  (1, '3 days', 1.10, 0.025, 1.00*0.325/0.30),
-  (2, '7 days', 1.00, 0.05, 1.00)
+  (1, '7 days', 0.05, 1.00, 1.00),
+  (2, '7 days', 0.025, 1.00, 1.00*0.325/0.30),
+  (3, '3 days', 0.05, 1.10, 1.00),
+  (4, '3 days', 0.025, 1.10, 1.00*0.325/0.30)
 )
 
-select time_period, time_price_ratio, '|', deductible, deductible_price_ratio
+select time_period, deductible, '|', time_price_ratio, deductible_price_ratio, '|', time_price_ratio * deductible_price_ratio as price_ratio
 from pricing_setup
 order by id
