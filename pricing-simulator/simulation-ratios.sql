5b4552c83bbe77877473b2e6ac9f4e5e045d4efd
tomfutago <tomfutago@gmail.com>
2025-06-20 10:36:34 +0100

    fix: pct for table output

--- pricing-simulator/simulation-ratios.sql
+++ pricing-simulator/simulation-ratios.sql
@@ -63,15 +63,15 @@ with simulation_results as (
   )
 
   select
-    100.0000 * sum(depeg_10) / count(depeg_10) as p_depeg_10,
-    100.0000 * sum(depeg_15) / count(depeg_15) as p_depeg_15,
-    100.0000 * sum(depeg_20) / count(depeg_20) as p_depeg_20,
-    100.0000 * sum(perm_depeg_10) / count(perm_depeg_10) as p_perm_depeg_10,
-    100.0000 * sum(perm_depeg_15) / count(perm_depeg_15) as p_perm_depeg_15,
-    100.0000 * sum(perm_depeg_20) / count(perm_depeg_20) as p_perm_depeg_20,
-    100.0000 * sum(loss_event_10) / count(loss_event_10) as p_loss_event_10,
-    100.0000 * sum(loss_event_15) / count(loss_event_15) as p_loss_event_15,
-    100.0000 * sum(loss_event_20) / count(loss_event_20) as p_loss_event_20
+    1.0000 * sum(depeg_10) / count(depeg_10) as p_depeg_10,
+    1.0000 * sum(depeg_15) / count(depeg_15) as p_depeg_15,
+    1.0000 * sum(depeg_20) / count(depeg_20) as p_depeg_20,
+    1.0000 * sum(perm_depeg_10) / count(perm_depeg_10) as p_perm_depeg_10,
+    1.0000 * sum(perm_depeg_15) / count(perm_depeg_15) as p_perm_depeg_15,
+    1.0000 * sum(perm_depeg_20) / count(perm_depeg_20) as p_perm_depeg_20,
+    1.0000 * sum(loss_event_10) / count(loss_event_10) as p_loss_event_10,
+    1.0000 * sum(loss_event_15) / count(loss_event_15) as p_loss_event_15,
+    1.0000 * sum(loss_event_20) / count(loss_event_20) as p_loss_event_20
   from simulation_probability_results
 
 )
