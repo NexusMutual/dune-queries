6db0ef7e827d05e95a50fdc1025ef31c0f8abf01
tomfutago <tomfutago@gmail.com>
2025-07-23 14:37:58 +0100

    feat: hack tx movements

---
+++ claims/loss-research/arcadia/hack-txs-erc20-movements.sql
@@ -0,0 +1,88 @@
+with
+
+address_to_check (cover_id, sub_account, hack_tx_hash) as (
+  values
+  (2704, 0x3AEC7Cf5C490475815D84351B81261B78B3e8edA, 0x28f028a72866ae11ca498de33ea93c27f996283caeea6eab8815d8ba0bd07c02),
+  (2704, 0xB44b2D73Ea2aD17A2A28a7272b3Bc60Fd2eB087a, 0xb4254354894732c635cef60b20572e3bcb0a8ea251220e6fec86e63619e820fe),
+  (2704, 0x5a06f887DF2016F5850D445F8c1739D4BD2c927C, 0x49548943f2d7703336813a59e60e70b77d00bf7e47523eaf5514951f992ef4b3),
+  (2704, 0x15E703949450D6A9Deca30a8C56212A9F9A26d28, 0x3e2629406f54b40fe50d21a6bd89f3530893cc1b1eb4b484835732220ee74e98),
+  (2704, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x2fa21ff0dd353eaea02be3db8c5e389746ce6f256469d4afd6c5b09b06aff483),
+  (2704, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x52115cf348559127a8b9e1eea49df37ffcfcd219d47c7ef4827832bde8e28b9b),
+  (2704, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x7510e28a5418623d0c2edff7b5a09eb4de05297288db07d676d054be91d7043b),
+  (2704, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0xa9cf136f396d04df6b3c6cd1d284bcf6345b6e5bb8d58ec29ce04e1a83025bd4),
+  (2704, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x0d3c80cf6aa6cde2d845dc84d8fac7c5dd38f4797b673b7f00388499056a1abd),
+  (2704, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0xb066f52bbaa2b52b2d3c15478cd043af8378deaf0da8aac6b7d5fab4af0aa13a),
+  (2705, 0x3AEC7Cf5C490475815D84351B81261B78B3e8edA, 0x28f028a72866ae11ca498de33ea93c27f996283caeea6eab8815d8ba0bd07c02),
+  (2705, 0xB44b2D73Ea2aD17A2A28a7272b3Bc60Fd2eB087a, 0xb4254354894732c635cef60b20572e3bcb0a8ea251220e6fec86e63619e820fe),
+  (2705, 0x5a06f887DF2016F5850D445F8c1739D4BD2c927C, 0x49548943f2d7703336813a59e60e70b77d00bf7e47523eaf5514951f992ef4b3),
+  (2705, 0x15E703949450D6A9Deca30a8C56212A9F9A26d28, 0x3e2629406f54b40fe50d21a6bd89f3530893cc1b1eb4b484835732220ee74e98),
+  (2705, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x2fa21ff0dd353eaea02be3db8c5e389746ce6f256469d4afd6c5b09b06aff483),
+  (2705, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x52115cf348559127a8b9e1eea49df37ffcfcd219d47c7ef4827832bde8e28b9b),
+  (2705, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x7510e28a5418623d0c2edff7b5a09eb4de05297288db07d676d054be91d7043b),
+  (2705, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0xa9cf136f396d04df6b3c6cd1d284bcf6345b6e5bb8d58ec29ce04e1a83025bd4),
+  (2705, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0x0d3c80cf6aa6cde2d845dc84d8fac7c5dd38f4797b673b7f00388499056a1abd),
+  (2705, 0xCB4d83D99Df0bd0171c22ae085a30f5e7609e1a8, 0xb066f52bbaa2b52b2d3c15478cd043af8378deaf0da8aac6b7d5fab4af0aa13a),
+  (2620, 0x0470459C8Fe08BC88602F81dF2C2eE3f4C38CF23, 0xee126e2593a1814621352313fd2b16e7868bc6b3121ff46799496da9ea0cc77f),
+  (2620, 0x876fDCA6cc8CE5B240b1E0E4E13C73a51ab2a0b5, 0x505c9b41bedf592de805e808d6082726d0961f31964c65a59dcbaa178b3826ae),
+  (2620, 0xE9De28C259B4A91123A3fa721bd161D567c22954, 0xd98552228c218d4c9a94432d6bc96aee19b44f90a114e70ddcf7c6cd36a3deb2),
+  (2620, 0x3CfCF5Ce951565f9EDD3Cd421D8C3Bf3e4FeDe99, 0xccf56232dc9fcfa27d0d68993dee1d5c0440e50fbb19f7e976e1f5f4ed6b7ea7),
+  (2620, 0xa95722Eb13b48Ef43302fA8DaE26338120574eA1, 0xa70c399894391ed34dd89558082960c050bb278ff2fbc3abcc0344fd9a4c86be),
+  (2620, 0x3F5Bb3F14488b72a12fA87603E002765413604A3, 0x05e20b4ab927c32942c3d206acf9652714dedb7894a10390c2fd395381781c70),
+  (2620, 0x3F5Bb3F14488b72a12fA87603E002765413604A3, 0xa5fec119d7a1be9df8d7f94c45dd18c9c987e35c87a2dbeaaeb09abb1cbcac6c),
+  (2620, 0x3F5Bb3F14488b72a12fA87603E002765413604A3, 0x5348c0b6955556007e966a6b67ce3a20d1fb32d4d3a56ec787d35ba5a6ebf728)
+),
+
+asset_movements as (
+  select
+    a.cover_id,
+    a.sub_account,
+    e.evt_block_time as block_time,
+    e.evt_block_number as block_number,
+    'out' as flow_type,
+    e.contract_address,
+    e.evt_tx_from,
+    e.evt_tx_to,
+    e."from",
+    e."to",
+    e.value,
+    e.evt_index,
+    e.evt_tx_hash as tx_hash
+  from erc20_base.evt_transfer e
+    inner join address_to_check a on e."from" = a.sub_account and e.evt_tx_hash = a.hack_tx_hash
+  union all
+  select
+    a.cover_id,
+    a.sub_account,
+    e.evt_block_time as block_time,
+    e.evt_block_number as block_number,
+    'in' as flow_type,
+    e.contract_address,
+    e.evt_tx_from,
+    e.evt_tx_to,
+    e."from",
+    e."to",
+    e.value,
+    e.evt_index,
+    e.evt_tx_hash as tx_hash
+  from erc20_base.evt_transfer e
+    inner join address_to_check a on e.to = a.sub_account and e.evt_tx_hash = a.hack_tx_hash
+)
+
+select distinct
+  m.cover_id,
+  m.sub_account,
+  m.block_time,
+  m.block_number,
+  m.flow_type,
+  m.contract_address,
+  t.symbol,
+  m.value / power(t.decimals, 10) as amount,
+  m.evt_index,
+  m.evt_tx_from,
+  m.evt_tx_to,
+  m."from",
+  m."to",
+  m.tx_hash
+from asset_movements m
+  left join tokens.erc20 t on m.contract_address = t.contract_address and t.blockchain = 'base'
+order by 1, 2, 3, 5
