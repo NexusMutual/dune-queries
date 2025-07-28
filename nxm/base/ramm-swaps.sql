ea9bf72e48d4c1ed00b709af5d8b65e6478cfeb6
tomfutago <tomfutago@gmail.com>
2025-07-28 14:25:19 +0100

    feat: ramm swaps base query

---
+++ nxm/base/ramm-swaps.sql
@@ -0,0 +1,21 @@
+select
+  block_time,
+  block_number,
+  'below' as flow_type,
+  transfer_from as user,
+  amount, -- NXM burned (technically negative)
+  tx_hash
+from query_5531182 -- nxm transfers - base
+where tx_to = 'NM: RAMM'
+  and transfer_to = 'origincity.eth' -- burn address for swaps below
+union all
+select
+  block_time,
+  block_number,
+  'above' as flow_type,
+  transfer_to as user,
+  -1 * amount as amount, -- NXM minted (technically positive)
+  tx_hash
+from query_5531182 -- nxm transfers - base
+where tx_to = 'NM: RAMM'
+  and transfer_from = 'origincity.eth' -- burn address for swaps above
