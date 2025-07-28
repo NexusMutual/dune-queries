b19c891d5760da9bcd357b152ff217cf9667c807
tomfutago <tomfutago@gmail.com>
2025-07-28 14:58:15 +0100

    feat: add block_date

--- nxm/base/ramm-swaps.sql
+++ nxm/base/ramm-swaps.sql
@@ -1,6 +1,7 @@
 select
   block_time,
   block_number,
+  date_trunc('day', block_time) as block_date,
   'below' as flow_type,
   transfer_from as user,
   amount, -- NXM burned (technically negative)
@@ -12,6 +13,7 @@ union all
 select
   block_time,
   block_number,
+  date_trunc('day', block_time) as block_date,
   'above' as flow_type,
   transfer_to as user,
   -1 * amount as amount, -- NXM minted (technically positive)
--- nxm/ramm-movement-overview.sql
+++ nxm/ramm-movement-overview.sql
@@ -1,16 +1,16 @@
 select
   user,
   sum(amount) as amount,
-  min(block_time) as first_swap,
-  max(block_time) as last_swap,
+  min(block_date) as first_swap,
+  max(block_date) as last_swap,
   count(distinct tx_hash) as swap_count,
   avg(amount) as mean_amount,
   approx_percentile(amount, 0.5) as median_amount,
-  sum(amount) filter (where block_time >= current_date - interval '365' day) as amount_365d,
-  sum(amount) filter (where block_time >= current_date - interval '180' day) as amount_180d,
-  sum(amount) filter (where block_time >= current_date - interval '90' day) as amount_90d,
-  sum(amount) filter (where block_time >= current_date - interval '30' day) as amount_30d,
-  sum(amount) filter (where block_time >= current_date - interval '7' day) as amount_7d
+  sum(amount) filter (where block_date >= current_date - interval '365' day) as amount_365d,
+  sum(amount) filter (where block_date >= current_date - interval '180' day) as amount_180d,
+  sum(amount) filter (where block_date >= current_date - interval '90' day) as amount_90d,
+  sum(amount) filter (where block_date >= current_date - interval '30' day) as amount_30d,
+  sum(amount) filter (where block_date >= current_date - interval '7' day) as amount_7d
 from query_5547032 -- ramm swaps - base
 group by 1
 order by 2 desc
