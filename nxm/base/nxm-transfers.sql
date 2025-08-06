27858ca2f5bad4b409bff08b77ffec82787541ed
tomfutago <tomfutago@gmail.com>
2025-08-06 14:11:55 +0100

    feat: add wnxm

--- nxm/base/nxm-transfers.sql
+++ nxm/base/nxm-transfers.sql
@@ -4,10 +4,11 @@ address_labels as (
   select address, address_label from query_5534312
 ),
 
-nxm_transfers as (
+transfers as (
   select
     evt_block_time as block_time,
     evt_block_number as block_number,
+    'NXM' as token,
     evt_tx_from as tx_from,
     evt_tx_to as tx_to,
     "from" as transfer_from,
@@ -16,12 +17,28 @@ nxm_transfers as (
     evt_index,
     evt_tx_hash as tx_hash
   from nexusmutual_ethereum.NXMToken_evt_Transfer
+  where value > 0
+  union all
+  select
+    evt_block_time as block_time,
+    evt_block_number as block_number,
+    'wNXM' as token,
+    evt_tx_from as tx_from,
+    evt_tx_to as tx_to,
+    "from" as transfer_from,
+    "to" as transfer_to,
+    value / 1e18 as amount,
+    evt_index,
+    evt_tx_hash as tx_hash
+  from wnxm_ethereum.wnxm_evt_transfer
+  where value > 0
 ),
 
-nxm_transfers_labelled as (
+transfers_labelled as (
   select
     t.block_time,
     t.block_number,
+    t.token,
     t.amount,
     coalesce(l_manual_tx_from.address_label, l_ens_tx_from.name, cast(t.tx_from as varchar)) as tx_from,
     coalesce(l_manual_tx_to.address_label, l_ens_tx_to.name, cast(t.tx_to as varchar)) as tx_to,
@@ -29,7 +46,7 @@ nxm_transfers_labelled as (
     coalesce(l_manual_transfer_to.address_label, l_ens_transfer_to.name, cast(t.transfer_to as varchar)) as transfer_to,
     t.evt_index,
     t.tx_hash
-  from nxm_transfers t
+  from transfers t
     left join labels.ens l_ens_tx_from on t.tx_from = l_ens_tx_from.address
     left join address_labels l_manual_tx_from on t.tx_from = l_manual_tx_from.address
     left join labels.ens l_ens_tx_to on t.tx_to = l_ens_tx_to.address
@@ -43,6 +60,7 @@ nxm_transfers_labelled as (
 select
   block_time,
   block_number,
+  token,
   tx_from,
   tx_to,
   transfer_from,
@@ -50,5 +68,5 @@ select
   amount,
   evt_index,
   tx_hash
-from nxm_transfers_labelled
+from transfers_labelled
 order by 1
