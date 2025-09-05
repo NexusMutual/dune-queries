bd5d998f9e9978aa0112d52403394d4792c8991d
tomfutago <tomfutago@gmail.com>
2025-09-05 13:55:22 +0100

    chore: split models

--- ramm/_development/monthly-staking-apy-selected-pools.sql
+++ ramm/_development/monthly-staking-apy-selected-pools.sql
@@ -1,57 +1,11 @@
-with
-
-staking_pool_names (pool_id, pool_name) as (
-  values
-    (2, 'Hugh'),
-    (22, 'BND')
-),
-
-staking_apy_current_state as (
-  select
-    block_date,
-    pool_id,
-    apy_30d_ma
-  from query_5729587 -- daily staking apy (2+22)
-),
-
-staking_apy_with_ramm as (
-  select
-    month_date,
-    pool_id,
-    apy
-  from query_5733434 -- daily staking apy with ramm (2+22)
-),
-
-daily_as_month_end as (
-  select
-    cast(date_trunc('month', block_date) as date) as month_date,
-    pool_id,
-    max(block_date) as month_end_date,
-    max_by(apy_30d_ma, block_date) as apy_30d_ma_month_end
-  from staking_apy_current_state
-  group by 1, 2
-),
-
-staking_apy_combined as (
-  select
-    month_date,
-    pool_id,
-    'current' as series,
-    apy_30d_ma_month_end as apy
-  from daily_as_month_end
-  union all
-  select
-    month_date,
-    pool_id,
-    'with ramm' as series,
-    apy
-  from staking_apy_with_ramm
-)
-
 select
-  sc.month_date,
-  concat(spn.pool_name, ' (', sc.series, ')') as series,
-  sc.apy
-from staking_apy_combined sc
-  inner join staking_pool_names spn on sc.pool_id = spn.pool_id
+  month_date,
+  pool_id,
+  pool_name,
+  total_staked_nxm,
+  nxm_reward_total,
+  apy
+from query_5733364 -- monthly staking apy with ramm
+where pool_id in (2, 22)
+  and month_date >= timestamp '2024-01-01'
 order by 1, 2
