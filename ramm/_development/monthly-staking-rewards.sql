393cd2267717db1e6f0b4f3e4f8dda064ef098f6
tomfutago <tomfutago@gmail.com>
2025-09-12 10:42:31 +0100

    feat: add 3m-ma rewards

--- ramm/_development/monthly-staking-rewards.sql
+++ ramm/_development/monthly-staking-rewards.sql
@@ -4,7 +4,10 @@ select
   pool_name,
   nxm_reward_total,
   eth_reward_total,
-  usd_reward_total
+  usd_reward_total,
+  avg(nxm_reward_total) over (partition by pool_id order by month_date rows between 2 preceding and current row) as nxm_reward_3m_moving_avg,
+  avg(eth_reward_total) over (partition by pool_id order by month_date rows between 2 preceding and current row) as eth_reward_3m_moving_avg,
+  avg(usd_reward_total) over (partition by pool_id order by month_date rows between 2 preceding and current row) as usd_reward_3m_moving_avg
 from query_4248623 -- staking rewards monthly
 where month_date >= timestamp '2024-01-01'
 order by 1, 2
