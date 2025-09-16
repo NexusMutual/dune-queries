d765a6d799d1127028f1d53554e8916e1560db96
tomfutago <tomfutago@gmail.com>
2025-09-16 10:34:29 +0100

    chore: switch capital_pool_totals spell to query

--- covers/wallet-monitoring/capital-pool-totals.sql
+++ covers/wallet-monitoring/capital-pool-totals.sql
@@ -4,6 +4,6 @@ select
   avg_capital_pool_eth_total,
   avg_capital_pool_usd_total,
   current_timestamp as inserted_at
-from nexusmutual_ethereum.capital_pool_totals
+from query_4627588 -- Capital Pool - base root
 where block_date >= timestamp '2024-12-01'
 order by 1
