7ab67ea6e31455a030ce803db378c113c56ac6a2
tomfutago <tomfutago@gmail.com>
2025-09-18 15:23:47 +0100

    refactor: account for swaps above

--- ramm/base/ramm-swaps.sql
+++ ramm/base/ramm-swaps.sql
@@ -14,21 +14,39 @@ book_value as (
 swaps_below as (
   select
     date_trunc('day', block_time) as block_date,
-    sum(amount_in) as nxm_in,
-    sum(amount_out) as eth_out,
-    avg(swap_price) as nxm_eth_swap_price
-  from query_4498669 -- RAMM swaps - base
+    sum(amount_in) as nxm_in_below,
+    sum(amount_out) as eth_out_below,
+    avg(swap_price) as nxm_eth_swap_price_below
+  from query_4498669 -- RAMM swap events - base
   where swap_type = 'below'
+  group by 1
+),
+
+swaps_above as (
+  select
+    date_trunc('day', block_time) as block_date,
+    sum(amount_out) as nxm_out_above,
+    sum(amount_in) as eth_in_above,
+    avg(swap_price) as nxm_eth_swap_price_above
+  from query_4498669 -- RAMM swap events - base
+  where swap_type = 'above'
   group by 1
 )
 
 select
   bv.block_date,
-  s.nxm_in,
-  s.eth_out,
-  s.nxm_eth_swap_price,
-  s.nxm_eth_swap_price * bv.avg_eth_usd_price as nxm_usd_swap_price,
+  sb.nxm_in_below,
+  sb.eth_out_below,
+  sb.nxm_eth_swap_price_below,
+  sb.nxm_eth_swap_price_below * bv.avg_eth_usd_price as nxm_usd_swap_price_below,
+  sa.nxm_out_above,
+  sa.eth_in_above,
+  sa.nxm_eth_swap_price_above,
+  sa.nxm_eth_swap_price_above * bv.avg_eth_usd_price as nxm_usd_swap_price_above,
   bv.eth_book_value,
-  bv.usd_book_value
+  bv.usd_book_value,
+  bv.avg_eth_usd_price
 from book_value bv
-  inner join swaps_below s on bv.block_date = s.block_date
+  left join swaps_below sb on bv.block_date = sb.block_date
+  left join swaps_above sa on bv.block_date = sa.block_date
+where bv.block_date >= cast('2023-11-28' as date) -- RAMM go live date
