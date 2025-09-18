8bf00eab918c041b2ed6c1ddcc608abe67798800
tomfutago <tomfutago@gmail.com>
2025-09-18 16:53:04 +0100

    feat: add swaps above

--- ramm/ramm-swaps.sql
+++ ramm/ramm-swaps.sql
@@ -3,28 +3,26 @@ with
 swaps_x_bv as (
   select
     block_date,
-    nxm_in,
-    eth_out,
-    nxm_eth_swap_price,
-    nxm_usd_swap_price,
+    nxm_eth_swap_price_below,
+    nxm_usd_swap_price_below,
+    nxm_eth_swap_price_above,
+    nxm_usd_swap_price_above,
     eth_book_value,
     usd_book_value,
-    if('{{currency}}' = 'USD', nxm_usd_swap_price, nxm_eth_swap_price) as swap_price,
-    if('{{currency}}' = 'USD', usd_book_value, eth_book_value) as book_value
-  from query_4516074 -- RAMM swaps below - base
+    if('{{currency}}' = 'usd', nxm_usd_swap_price_below, nxm_eth_swap_price_below) as swap_price_below,
+    if('{{currency}}' = 'usd', nxm_usd_swap_price_above, nxm_eth_swap_price_above) as swap_price_above,
+    if('{{currency}}' = 'usd', usd_book_value, eth_book_value) as book_value
+  from query_4516074 -- RAMM swaps - base
 )
 
 select
   block_date,
-  nxm_in,
-  eth_out,
-  nxm_eth_swap_price,
-  nxm_usd_swap_price,
-  eth_book_value,
-  usd_book_value,
-  swap_price,
+  swap_price_below,
+  swap_price_above,
   book_value,
-  swap_price / book_value as redemption_pct,
-  1.00 - (swap_price / book_value) as discount
+  swap_price_below / book_value as redemption_pct_below,
+  1.00 - (swap_price_below / book_value) as discount_below,
+  swap_price_above / book_value as redemption_pct_above,
+  1.00 - (swap_price_above / book_value) as discount_above
 from swaps_x_bv
 order by 1 desc
