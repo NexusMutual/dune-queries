7fd52e9ef898392a6302fe2097f678bb1c14a4a4
tomfutago <tomfutago@gmail.com>
2025-09-19 14:33:43 +0100

    fix: refs

--- ramm/redemption-avg-discount-bv.sql
+++ ramm/redemption-avg-discount-bv.sql
@@ -1,2 +1,2 @@
-select sum((1 - (eth_out / nxm_in / eth_book_value)) * nxm_in) / sum(nxm_in) * 100 as avg_discount_bv_pct
-from query_4516074 -- RAMM swaps below - base
+select sum((1 - (eth_out_below / nxm_in_below / eth_book_value)) * nxm_in_below) / sum(nxm_in_below) * 100 as avg_discount_bv_pct
+from query_4516074 -- RAMM swaps - base
